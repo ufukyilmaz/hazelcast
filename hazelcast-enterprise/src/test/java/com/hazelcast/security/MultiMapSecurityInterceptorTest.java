8d71535eb83ff01f30e071f37308ad9125a5013f
Mesut Celik <mesut@hazelcast.com>
2014-08-15 17:08:32 +0300

    fixed problematic tests

--- hazelcast-enterprise/src/test/java/com/hazelcast/security/MultiMapSecurityInterceptorTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/security/MultiMapSecurityInterceptorTest.java
@@ -4,10 +4,8 @@ import com.hazelcast.core.EntryAdapter;
 import com.hazelcast.core.MultiMap;
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
 import com.hazelcast.multimap.impl.MultiMapService;
-import com.hazelcast.test.annotation.ProblematicTest;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.util.concurrent.TimeUnit;
@@ -73,7 +71,6 @@ public class MultiMapSecurityInterceptorTest extends BaseInterceptorTest {
     }
 
     @Test
-    @Category(ProblematicTest.class)
     public void containsKey() {
         final String key = randomString();
         multiMap.containsKey(key);
@@ -88,7 +85,6 @@ public class MultiMapSecurityInterceptorTest extends BaseInterceptorTest {
     }
 
     @Test
-    @Category(ProblematicTest.class)
     public void containsEntry() {
         final String key = randomString();
         final String val = randomString();

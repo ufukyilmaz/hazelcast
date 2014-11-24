604bbcb2cc2da02a72229bb44fcc195376bd11e6
serkanozal <serkan@hazelcast.com>
2014-11-24 14:12:50 +0200

    MemoryBlockProcessor created for handling memory accessing, allocating and disposing from one point so memory usage can be monitored

---
+++ hazelcast-enterprise/src/main/java/com/hazelcast/memory/MemoryBlockProcessor.java
@@ -0,0 +1,19 @@
+package com.hazelcast.memory;
+
+import com.hazelcast.nio.serialization.Data;
+import com.hazelcast.nio.serialization.DataType;
+
+/**
+ * @author sozal 17/11/14
+ */
+public interface MemoryBlockProcessor<V extends MemoryBlock>
+        extends MemoryBlockAccessor<V> {
+
+    Data toData(Object obj, DataType dataType);
+    Data convertData(Data data, DataType dataType);
+    void disposeData(Data data);
+
+    long allocate(long size);
+    void free(long address, long size);
+
+}

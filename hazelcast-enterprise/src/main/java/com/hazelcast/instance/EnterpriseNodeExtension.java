c1587d87927687f0098b7fcca454418d4a277efe
ibrahim gürses <ibrahim@hazelcast.com>
2015-03-26 15:22:53 +0200

    Memory config check is done before calculating total memory.

--- hazelcast-enterprise/src/main/java/com/hazelcast/instance/EnterpriseNodeExtension.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/instance/EnterpriseNodeExtension.java
@@ -346,10 +346,12 @@ public class EnterpriseNodeExtension extends DefaultNodeExtension implements Nod
     @Override
     public void beforeJoin() {
         NativeMemoryConfig memoryConfig = node.getConfig().getNativeMemoryConfig();
+        if (!memoryConfig.isEnabled())
+            return;
         long totalNativeMemorySize = node.getClusterService().getSize()
                 * memoryConfig.getSize().bytes();
         long licensedNativeMemorySize = MemoryUnit.GIGABYTES.toBytes(license.getAllowedNativeMemorySize());
-        if (memoryConfig.isEnabled() && totalNativeMemorySize >= licensedNativeMemorySize) {
+        if ( totalNativeMemorySize >= licensedNativeMemorySize) {
             throw new InvalidLicenseException("Total native memory of cluster exceeds licensed native memory. "
                     + "Please contact sales@hazelcast.com");
         }

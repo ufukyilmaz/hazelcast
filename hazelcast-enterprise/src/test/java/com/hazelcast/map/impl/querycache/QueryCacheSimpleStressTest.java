0effb5d9112ebf0410ed10c94c20a92347f16403
Ahmet MIRCIK <ahmet@hazelcast.com>
2015-05-11 10:32:44 +0300

    fix attempt for query cache simple stress test

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/querycache/QueryCacheSimpleStressTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/querycache/QueryCacheSimpleStressTest.java
@@ -1,17 +1,22 @@
 package com.hazelcast.map.impl.querycache;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.QueryCacheConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IEnterpriseMap;
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
 import com.hazelcast.instance.GroupProperties;
+import com.hazelcast.map.EventLostEvent;
 import com.hazelcast.map.QueryCache;
+import com.hazelcast.map.listener.EventLostListener;
 import com.hazelcast.query.TruePredicate;
 import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
+import com.hazelcast.test.annotation.Repeat;
 import com.hazelcast.test.annotation.SlowTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +27,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(EnterpriseSerialJUnitClassRunner.class)
 @Category(SlowTest.class)
+@Repeat(100)
 public class QueryCacheSimpleStressTest extends HazelcastTestSupport {
 
     private final String mapName = randomString();
@@ -31,6 +37,12 @@ public class QueryCacheSimpleStressTest extends HazelcastTestSupport {
 
     @Before
     public void setUp() throws Exception {
+
+        EvictionConfig evictionConfig = new EvictionConfig();
+        evictionConfig.setMaximumSizePolicy(EvictionConfig.MaxSizePolicy.ENTRY_COUNT);
+        evictionConfig.setSize(Integer.MAX_VALUE);
+        evictionConfig.setEvictionPolicy(EvictionPolicy.LRU);
+
         QueryCacheConfig queryCacheConfig = new QueryCacheConfig(cacheName);
         queryCacheConfig
                 .setBufferSize(30)
@@ -38,18 +50,12 @@ public class QueryCacheSimpleStressTest extends HazelcastTestSupport {
                 .setBatchSize(2)
                 .setPopulate(true)
                 .getPredicateConfig().setImplementation(TruePredicate.INSTANCE);
+        queryCacheConfig.setEvictionConfig(evictionConfig);
 
         MapConfig mapConfig = new MapConfig(mapName);
         mapConfig.addQueryCacheConfig(queryCacheConfig);
         config.addMapConfig(mapConfig);
         config.setProperty(GroupProperties.PROP_ELASTIC_MEMORY_ENABLED, "false");
-    }
-
-    private <K, V> IEnterpriseMap<K, V> getMap() {
-        TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(3);
-        HazelcastInstance[] instances = factory.newInstances(config);
-        HazelcastInstance node = instances[0];
-        return (IEnterpriseMap) node.getMap(mapName);
     }
 
     @Test
@@ -68,11 +74,24 @@ public class QueryCacheSimpleStressTest extends HazelcastTestSupport {
         Thread thread = new Thread(runnable);
         thread.start();
 
-        QueryCache<Integer, Integer> queryCache = map.getQueryCache(cacheName, TruePredicate.INSTANCE, true);
+        final QueryCache<Integer, Integer> queryCache = map.getQueryCache(cacheName, TruePredicate.INSTANCE, true);
+        queryCache.addEntryListener(new EventLostListener() {
+            @Override
+            public void eventLost(EventLostEvent event) {
+                queryCache.tryRecover();
+            }
+        }, true);
 
         thread.join();
 
         assertQueryCacheSizeEventually(numberOfElementsToPut, queryCache);
+    }
+
+    private <K, V> IEnterpriseMap<K, V> getMap() {
+        TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(3);
+        HazelcastInstance[] instances = factory.newInstances(config);
+        HazelcastInstance node = instances[0];
+        return (IEnterpriseMap) node.getMap(mapName);
     }
 
     private void assertQueryCacheSizeEventually(final int expected, final QueryCache queryCache) {

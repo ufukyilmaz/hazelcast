167a6b910d4e648c8062f97221e0b50d459874c2
serkanozal <serkan@hazelcast.com>
2015-08-10 14:54:55 +0300

    Native key should be disposed if OOME is thrown during map expand.

--- hazelcast-enterprise/src/main/java/com/hazelcast/elastic/map/BinaryElasticHashMap.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/elastic/map/BinaryElasticHashMap.java
@@ -10,10 +10,11 @@ import com.hazelcast.memory.NativeOutOfMemoryError;
 import com.hazelcast.nio.UnsafeHelper;
 import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.nio.serialization.DataType;
-import com.hazelcast.nio.serialization.impl.HeapData;
 import com.hazelcast.nio.serialization.EnterpriseSerializationService;
+import com.hazelcast.nio.serialization.impl.HeapData;
 import com.hazelcast.nio.serialization.impl.NativeMemoryData;
 import com.hazelcast.nio.serialization.impl.NativeMemoryDataUtil;
+import com.hazelcast.util.ExceptionUtil;
 import com.hazelcast.util.HashUtil;
 import sun.misc.Unsafe;
 
@@ -215,10 +216,24 @@ public class BinaryElasticHashMap<V extends MemoryBlock> implements ElasticMap<D
         }
 
         NativeMemoryData memKey = (NativeMemoryData) memoryBlockProcessor.convertData(key, DataType.NATIVE);
+
+        assert memKey.address() != MemoryAllocator.NULL_ADDRESS : "Null key!";
+        assert value.address() != MemoryAllocator.NULL_ADDRESS : "Null value!";
+
         // Check if we need to grow. If so, reallocate new data, fill in the last element
         // and rehash.
         if (assigned == resizeAt) {
-            expandAndPut(memKey.address(), value.address(), slot);
+            try {
+                expandAndPut(memKey.address(), value.address(), slot);
+            } catch (Throwable error) {
+                // If they are not same, this means that the key is converted to native memory data at here.
+                // So, it must be disposed at here
+                if (memKey != key) {
+                    memoryBlockProcessor.disposeData(memKey);
+                }
+                throw ExceptionUtil.rethrow(error);
+            }
+
         } else {
             assigned++;
             setKey(slot, memKey.address());

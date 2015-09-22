522925b4c9dc54b2c8c4a08b6aeea953adb3168c
sancar <sancar@hazelcast.com>
2015-09-22 11:13:29 +0300

    cache listener security test fix

--- hazelcast-enterprise-client-new/src/test/java/com/hazelcast/client/security/CacheSecurityInterceptorTest.java
+++ hazelcast-enterprise-client-new/src/test/java/com/hazelcast/client/security/CacheSecurityInterceptorTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2008-2013, Hazelcast, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.hazelcast.client.security;
 
 import com.hazelcast.cache.CacheContextTest;
@@ -266,24 +251,24 @@ public class CacheSecurityInterceptorTest extends BaseInterceptorTest {
     @Test
     public void registerCacheEntryListener() {
 
-        CacheEntryListenerConfiguration<String, String> cacheEntryListenerConfig =
+        CacheEntryListenerConfiguration<String, String> config =
                 new MutableCacheEntryListenerConfiguration<String, String>(
                         FactoryBuilder.factoryOf(new CacheContextTest.TestListener()), null, true, true);
 
-        interceptor.setExpectation(getObjectType(), getNameWithPrefix(), "registerCacheEntryListener", new Object[]{});
-        cache.registerCacheEntryListener(cacheEntryListenerConfig);
+        interceptor.setExpectation(getObjectType(), getNameWithPrefix(), "registerCacheEntryListener", null);
+        cache.registerCacheEntryListener(config);
     }
 
     @Test
     public void deregisterCacheEntryListener() {
-        CacheEntryListenerConfiguration<String, String> cacheEntryListenerConfig =
+        CacheEntryListenerConfiguration<String, String> config =
                 new MutableCacheEntryListenerConfiguration<String, String>(
                         FactoryBuilder.factoryOf(new CacheContextTest.TestListener()), null, true, true);
 
 
-        cache.registerCacheEntryListener(cacheEntryListenerConfig);
-        interceptor.setExpectation(getObjectType(), getNameWithPrefix(), "deregisterCacheEntryListener", new Object[]{});
-        cache.deregisterCacheEntryListener(cacheEntryListenerConfig);
+        cache.registerCacheEntryListener(config);
+        interceptor.setExpectation(getObjectType(), getNameWithPrefix(), "deregisterCacheEntryListener", null);
+        cache.deregisterCacheEntryListener(config);
     }
 
     @Test

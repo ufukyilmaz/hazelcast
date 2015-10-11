ufukyilmaz/hazelcast

commit 95c629f2591bae05d48acf21e18b3a710d468f64

Log errors on `disposeInternal`

hazelcast-enterprise/src/main/java/com/hazelcast/cache/hidensity/operation/AbstractHiDensityCacheOperation.java
@@ -160,8 +160,8 @@ abstract class AbstractHiDensityCacheOperation
 
         try {
             disposeInternal(serializationService);
-        } catch (Throwable ignored) {
-            EmptyStatement.ignore(ignored);
+        } catch (Throwable e) {
+            getLogger().warning("Error while disposing internal...", e);
             // TODO ignored error at the moment
             // a double free() error may be thrown if an operation fails
             // since internally key/value references are freed on oome

commit 80b8b99209c29adc5292ac330fa5cee85b484042

Fix checkstyle issue introduced by commit 95c629f2591bae05d48acf21e18b3a710d468f64

hazelcast-enterprise/src/main/java/com/hazelcast/cache/hidensity/operation/AbstractHiDensityCacheOperation.java
@@ -17,7 +17,6 @@ import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.spi.BackupOperation;
 import com.hazelcast.spi.PartitionAwareOperation;
 import com.hazelcast.spi.impl.AbstractNamedOperation;
-import com.hazelcast.util.EmptyStatement;
 import com.hazelcast.util.ExceptionUtil;
 
 import java.io.IOException;
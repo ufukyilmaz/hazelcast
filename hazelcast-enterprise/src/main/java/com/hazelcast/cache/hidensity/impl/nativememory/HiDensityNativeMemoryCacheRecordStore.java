ufukyilmaz/hazelcast

commit 065246f93375e6f5edbec9200af02506d50dad01

Minor refactor on `HiDensityNativeMemoryCacheRecordStore::merge`

hazelcast-enterprise/src/main/java/com/hazelcast/cache/hidensity/impl/nativememory/HiDensityNativeMemoryCacheRecordStore.java
@@ -758,12 +758,12 @@ public class HiDensityNativeMemoryCacheRecordStore
                 merged = createRecordWithExpiry(key, value, expiryTime,
                                                 now, true, completionId, origin) != null;
             } else {
-                Object existingValue = record.getValue();
+                NativeMemoryData existingValue = record.getValue();
                 Object newValue =
-                        mergePolicy.merge(name, cacheEntryView,
+                        mergePolicy.merge(name, 
+                                          cacheEntryView,
                                           new DefaultCacheEntryView(key,
-                                                                    // TODO Should we pass heap data for safety?
-                                                                    toData(existingValue),
+                                                                    existingValue,
                                                                     record.getExpirationTime(),
                                                                     record.getAccessTime(),
                                                                     record.getAccessHit()));
@@ -771,7 +771,7 @@ public class HiDensityNativeMemoryCacheRecordStore
                     merged = updateRecordWithExpiry(key, newValue, record, expiryTime,
                                                     now, true, completionId, caller, origin);
                 }
-                publishEvent(createCacheCompleteEvent(toHeapData(cacheEntryView.getKey()),
+                publishEvent(createCacheCompleteEvent(toHeapData(key),
                                                       CacheRecord.EXPIRATION_TIME_NOT_AVAILABLE,
                                                       origin, completionId));
             }

commit 56a7831cfae238d22b3b2f831bb51fac297d6038

Fix checkstyle issue introduced by 065246f93375e6f5edbec9200af02506d50dad01

hazelcast-enterprise/src/main/java/com/hazelcast/cache/hidensity/impl/nativememory/HiDensityNativeMemoryCacheRecordStore.java
@@ -760,7 +760,7 @@ public class HiDensityNativeMemoryCacheRecordStore
             } else {
                 NativeMemoryData existingValue = record.getValue();
                 Object newValue =
-                        mergePolicy.merge(name, 
+                        mergePolicy.merge(name,
                                           cacheEntryView,
                                           new DefaultCacheEntryView(key,
                                                                     existingValue,
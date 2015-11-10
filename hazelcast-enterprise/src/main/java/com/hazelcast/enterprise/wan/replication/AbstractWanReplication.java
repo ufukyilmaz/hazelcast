ufukyilmaz/hazelcast

commit 8cd6ceb82d4adbe3d0346356b48eb681365a914f

cleanup

hazelcast-enterprise/src/main/java/com/hazelcast/enterprise/wan/replication/AbstractWanReplication.java
@@ -22,6 +22,7 @@ import com.hazelcast.nio.Address;
 import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.nio.serialization.DataSerializable;
 import com.hazelcast.partition.InternalPartition;
+import com.hazelcast.spi.InternalCompletableFuture;
 import com.hazelcast.spi.InvocationBuilder;
 import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.OperationService;
@@ -126,15 +127,10 @@ public abstract class AbstractWanReplication
         String serviceName = EnterpriseWanReplicationService.SERVICE_NAME;
         InvocationBuilder invocationBuilder
                 = operationService.createInvocationBuilder(serviceName, wanOperation, target);
-        Future<Boolean> future = invocationBuilder.setTryCount(1)
+        InternalCompletableFuture<Boolean> future = invocationBuilder.setTryCount(1)
                 .setCallTimeout(operationTimeout)
                 .invoke();
-        try {
-            return future.get();
-        } catch (Exception ex) {
-            ExceptionUtil.rethrow(ex);
-        }
-        return false;
+        return future.getSafely();
     }
 
     public void publishReplicationEvent(WanReplicationEvent wanReplicationEvent) {

hazelcast-enterprise/src/test/java/com/hazelcast/wan/map/AbstractMapWanReplicationTest.java
@@ -10,7 +10,6 @@ import com.hazelcast.map.merge.LatestUpdateMapMergePolicy;
 import com.hazelcast.map.merge.PassThroughMergePolicy;
 import com.hazelcast.map.merge.PutIfAbsentMapMergePolicy;
 import com.hazelcast.test.AssertTask;
-import com.hazelcast.test.annotation.Repeat;
 import com.hazelcast.wan.AbstractWanReplicationTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -43,7 +42,7 @@ public abstract class AbstractMapWanReplicationTest extends AbstractWanReplicati
         configC.getNetworkConfig().setPort(5901);
     }
 
-    private void setupReplicateFrom(Config fromConfig, Config toConfig, int clusterSz, String setupName, String policy) {
+    protected void setupReplicateFrom(Config fromConfig, Config toConfig, int clusterSz, String setupName, String policy) {
         WanReplicationConfig wanConfig = fromConfig.getWanReplicationConfig(setupName);
         if (wanConfig == null) {
             wanConfig = new WanReplicationConfig();
@@ -60,7 +59,7 @@ public abstract class AbstractMapWanReplicationTest extends AbstractWanReplicati
         fromConfig.getMapConfig("default").setWanReplicationRef(wanRef);
     }
 
-    private void createDataIn(HazelcastInstance[] cluster, String mapName, int start, int end) {
+    protected void createDataIn(HazelcastInstance[] cluster, String mapName, int start, int end) {
         HazelcastInstance node = getNode(cluster);
         IMap m = node.getMap(mapName);
         for (; start < end; start++) {
@@ -147,7 +146,7 @@ public abstract class AbstractMapWanReplicationTest extends AbstractWanReplicati
         }, ASSERT_TRUE_EVENTUALLY_TIMEOUT_VALUE);
     }
 
-    private void assertDataInFrom(final HazelcastInstance[] cluster, final String mapName, final int start, final int end, final String sourceGroupName) {
+    protected void assertDataInFrom(final HazelcastInstance[] cluster, final String mapName, final int start, final int end, final String sourceGroupName) {
         assertTrueEventually(new AssertTask() {
             public void run() {
                 sleepSeconds(10);
@@ -283,12 +282,6 @@ public abstract class AbstractMapWanReplicationTest extends AbstractWanReplicati
         assertDataInFrom(clusterC, "map", 0, 100, clusterA);
 
         assertDataSizeEventually(clusterC, "map", 200);
-
-        removeDataIn(clusterA, "map", 0, 100);
-        assertKeysNotIn(clusterC, "map", 0, 100);
-        removeDataIn(clusterB, "map", 100, 200);
-        assertKeysNotIn(clusterC, "map", 100, 200);
-        assertDataSizeEventually(clusterC, "map", 0);
     }
 
 
@@ -486,21 +479,6 @@ public abstract class AbstractMapWanReplicationTest extends AbstractWanReplicati
 
         assertDataSizeEventually(clusterA, "map", 0);
         assertDataSizeEventually(clusterB, "map", 0);
-    }
-
-    @Test
-    public void recoverAfterTargetClusterFailure() {
-        setupReplicateFrom(configA, configB, clusterB.length, "atob", PassThroughMergePolicy.class.getName());
-        startClusterA();
-
-        createDataIn(clusterA, "map", 0, 10000);
-
-        sleepSeconds(10);
-
-        clusterA[0].shutdown();
-        sleepSeconds(10);
-        startClusterB();
-        assertDataInFrom(clusterB, "map", 0, 10000, getNode(clusterA[1]).getConfig().getGroupConfig().getName());
     }
 
     @Test

hazelcast-enterprise/src/test/java/com/hazelcast/wan/map/MapWanNoDelayReplicationTest.java
@@ -1,7 +1,9 @@
 package com.hazelcast.wan.map;
 
 import com.hazelcast.enterprise.wan.replication.WanNoDelayReplication;
+import com.hazelcast.map.merge.PassThroughMergePolicy;
 import com.hazelcast.test.annotation.SlowTest;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(SlowTest.class)
@@ -10,5 +12,20 @@ public class MapWanNoDelayReplicationTest extends AbstractMapWanReplicationTest 
     @Override
     public String getReplicationImpl() {
         return WanNoDelayReplication.class.getName();
+    }
+
+    @Test
+    public void recoverAfterTargetClusterFailure() {
+        setupReplicateFrom(configA, configB, clusterB.length, "atob", PassThroughMergePolicy.class.getName());
+        startClusterA();
+
+        createDataIn(clusterA, "map", 0, 10000);
+
+        sleepSeconds(10);
+
+        clusterA[0].shutdown();
+        sleepSeconds(10);
+        startClusterB();
+        assertDataInFrom(clusterB, "map", 0, 10000, getNode(clusterA[1]).getConfig().getGroupConfig().getName());
     }
 }

commit 6f89146faed42c61319db6048a445e5ca12b258e

Fix checkstyle issue introduced by 8cd6ceb

hazelcast-enterprise/src/main/java/com/hazelcast/enterprise/wan/replication/AbstractWanReplication.java
@@ -35,7 +35,6 @@ import com.hazelcast.wan.WanReplicationPublisher;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
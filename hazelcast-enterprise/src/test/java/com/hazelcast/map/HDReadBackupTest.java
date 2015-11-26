7bcda502cbcf0503577bd4d81c5308053f50613a
Ahmet MIRCIK <ahmet@hazelcast.com>
2015-11-26 16:49:49 +0200

    Added HDLocalMapStatsTest and minor test renaming is done.

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/HDReadBackupTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDReadBackupTest.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hazelcast.map;
 
 
@@ -23,7 +7,6 @@ import com.hazelcast.core.IMap;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -31,14 +14,13 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.hazelcast.config.NativeMemoryConfig.MemoryAllocatorType.POOLED;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category(QuickTest.class)
-public class ReadBackupTest extends HazelcastTestSupport {
+public class HDReadBackupTest extends HazelcastTestSupport {
 
     IMap<String, String> map;
     String key = randomString();

d2197cd79bf67e7069f479f5b56a80dadf92820e
Marko Topolnik <marko@hazelcast.com>
2015-12-23 20:30:44 +0100

    Bugfix in Rebuilder + regression test

--- hazelcast-enterprise-hotrestart/src/test/java/com/hazelcast/spi/hotrestart/impl/HotRestartStoreQuickTest.java
+++ hazelcast-enterprise-hotrestart/src/test/java/com/hazelcast/spi/hotrestart/impl/HotRestartStoreQuickTest.java
@@ -1,9 +1,12 @@
 package com.hazelcast.spi.hotrestart.impl;
 
+import com.hazelcast.logging.LoggingService;
+import com.hazelcast.spi.hotrestart.impl.testsupport.MockStoreRegistry;
 import com.hazelcast.spi.hotrestart.impl.testsupport.TestProfile;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -13,7 +16,10 @@ import org.junit.runner.RunWith;
 import java.io.File;
 
 import static com.hazelcast.nio.IOUtil.delete;
+import static com.hazelcast.spi.hotrestart.impl.testsupport.HotRestartTestUtil.createLoggingService;
 import static com.hazelcast.spi.hotrestart.impl.testsupport.HotRestartTestUtil.hotRestartHome;
+import static com.hazelcast.spi.hotrestart.impl.testsupport.HotRestartTestUtil.logger;
+import static com.hazelcast.spi.hotrestart.impl.testsupport.HotRestartTestUtil.metricsRegistry;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
@@ -21,33 +27,89 @@ public class HotRestartStoreQuickTest {
 
     @Rule public final TestName testName = new TestName();
 
-    @Test public void onHeapTest() throws Exception {
-        quickTest(false);
+    private File testingHome;
+    private TestProfile profile;
+
+    @Before public void setup() {
+        testingHome = hotRestartHome(getClass(), testName);
+        delete(testingHome);
+        profile = new TestProfile.Default();
     }
 
-    @Test public void offHeapTest() throws Exception {
-        quickTest(true);
+    @Test public void onHeapExercise() throws Exception {
+        setupGeneralProfile();
+        profile.offHeapMb = 0;
+        generalExercise();
     }
 
-    private void quickTest(boolean offHeap) throws Exception {
-        final File testingHome = hotRestartHome(getClass(), testName);
-        final TestProfile p = new TestProfile.Default();
-        p.testCycleCount = 2;
-        p.exerciseTimeSeconds = 10;
-        p.clearIntervalSeconds = 4;
-        p.prefixCount = 10;
-        p.keysetSize = 30 * 1000;
-        p.hotSetFraction = 1;
-        p.logItersHotSetChange = 31;
-        p.logMinSize = 7;
-        p.sizeIncreaseSteps = 15;
-        p.logStepSize = 1;
-        p.offHeapMb = offHeap ? 1024 : 0;
-        p.offHeapMetadataPercentage = 12f;
-        p.restartCount = 0;
-        p.disableIo = false;
+    @Test public void offHeapExercise() throws Exception {
+        setupGeneralProfile();
+        profile.offHeapMb = 1024;
+        generalExercise();
+    }
+
+    @Test public void testRemoveAllKeys() throws Exception {
+        profile.keysetSize = 100;
+        final LoggingService loggingService = createLoggingService();
+        logger = loggingService.getLogger("hotrestart-test");
+        final HotRestartStoreConfig cfg = new HotRestartStoreConfig()
+                .setHomeDir(new File(testingHome, "hr-store"))
+                .setLoggingService(loggingService)
+                .setMetricsRegistry(metricsRegistry(loggingService));
+        MockStoreRegistry reg = new MockStoreRegistry(cfg, null);
+        putAll(reg);
+        removeAll(reg);
+        putAll(reg);
+        closeAndDispose(reg);
+        reg = new MockStoreRegistry(cfg, null);
+        removePutAll(reg);
+        closeAndDispose(reg);
+    }
+
+    private void putAll(MockStoreRegistry reg) {
+        final byte[] value = new byte[102400];
+        for (int key = 0; key < profile.keysetSize; key++) {
+            reg.put(1, key + 1, value);
+        }
+    }
+
+    private void removeAll(MockStoreRegistry reg) {
+        for (int key = 0; key < profile.keysetSize; key++) {
+            reg.remove(1, key + 1);
+        }
+    }
+
+    private void removePutAll(MockStoreRegistry reg) {
+        for (int i = 0; i < 3; i++) {
+            removeAll(reg);
+            putAll(reg);
+        }
+    }
+
+    private static void closeAndDispose(MockStoreRegistry reg) {
+        reg.closeHotRestartStore();
+        reg.disposeRecordStores();
+    }
+
+    private void setupGeneralProfile() {
+        profile.testCycleCount = 2;
+        profile.exerciseTimeSeconds = 10;
+        profile.clearIntervalSeconds = 4;
+        profile.prefixCount = 10;
+        profile.keysetSize = 30 * 1000;
+        profile.hotSetFraction = 1;
+        profile.logItersHotSetChange = 31;
+        profile.logMinSize = 7;
+        profile.sizeIncreaseSteps = 15;
+        profile.logStepSize = 1;
+        profile.offHeapMetadataPercentage = 12f;
+        profile.restartCount = 0;
+        profile.disableIo = false;
+    }
+
+    private void generalExercise() throws Exception {
         try {
-            new HotRestartStoreExerciser(testingHome, p).proceed();
+            new HotRestartStoreExerciser(testingHome, profile).proceed();
         } finally {
             delete(testingHome);
         }

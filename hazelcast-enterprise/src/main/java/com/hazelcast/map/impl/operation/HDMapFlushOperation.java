ee8ab0f334223454acb56d2193d128703090b2b0
Ahmet MIRCIK <ahmet@hazelcast.com>
2015-12-25 17:35:35 +0200

    Adds HDMapFlushBackupOperation

--- hazelcast-enterprise/src/main/java/com/hazelcast/map/impl/operation/HDMapFlushOperation.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/map/impl/operation/HDMapFlushOperation.java
@@ -1,9 +1,11 @@
 package com.hazelcast.map.impl.operation;
 
-import com.hazelcast.spi.PartitionAwareOperation;
+import com.hazelcast.config.MapStoreConfig;
+import com.hazelcast.spi.BackupAwareOperation;
+import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.impl.MutatingOperation;
 
-public class HDMapFlushOperation extends HDMapOperation implements PartitionAwareOperation, MutatingOperation {
+public class HDMapFlushOperation extends HDMapOperation implements BackupAwareOperation, MutatingOperation {
 
     public HDMapFlushOperation() {
     }
@@ -20,6 +22,29 @@ public class HDMapFlushOperation extends HDMapOperation implements PartitionAwar
     @Override
     public Object getResponse() {
         return true;
+    }
+
+    @Override
+    public boolean shouldBackup() {
+        MapStoreConfig mapStoreConfig = mapContainer.getMapConfig().getMapStoreConfig();
+        return mapStoreConfig != null
+                && mapStoreConfig.isEnabled()
+                && mapStoreConfig.getWriteDelaySeconds() > 0;
+    }
+
+    @Override
+    public int getAsyncBackupCount() {
+        return mapContainer.getAsyncBackupCount();
+    }
+
+    @Override
+    public int getSyncBackupCount() {
+        return mapContainer.getBackupCount();
+    }
+
+    @Override
+    public Operation getBackupOperation() {
+        return new HDMapFlushBackupOperation(name);
     }
 
 }

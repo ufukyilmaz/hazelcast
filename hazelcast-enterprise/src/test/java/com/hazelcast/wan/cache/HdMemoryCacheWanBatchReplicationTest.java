19b9a89a85f92f3db157120c00eacb7d73ddb5d0
Emrah Kocaman <emrahkocaman@gmail.com>
2015-12-28 16:05:54 +0200

    Remove @Ignore

--- hazelcast-enterprise/src/test/java/com/hazelcast/wan/cache/HdMemoryCacheWanBatchReplicationTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/wan/cache/HdMemoryCacheWanBatchReplicationTest.java
@@ -3,11 +3,9 @@ package com.hazelcast.wan.cache;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.enterprise.wan.replication.WanBatchReplication;
 import com.hazelcast.test.annotation.SlowTest;
-import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 
 @Category(SlowTest.class)
-@Ignore
 public class HdMemoryCacheWanBatchReplicationTest extends AbstractCacheWanReplicationTest {
 
     @Override

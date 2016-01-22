d17a7f557b5f743962dc13906dc784c013d3635f
İbrahim Gurses <ibrahim@hazelcast.com>
2016-01-22 10:05:54 +0200

    Update SampleLicense.java
    Updated LICENSE_WITH_DIFFERENT_VERSION.

--- hazelcast-enterprise/src/test/java/com/hazelcast/enterprise/SampleLicense.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/enterprise/SampleLicense.java
@@ -10,7 +10,7 @@ public interface SampleLicense {
     public static final String ENTERPRISE_LICENSE_WITHOUT_HUMAN_READABLE_PART = "IFEJwam5OUVBk6fyuT0rYi209DC92Dn91419Pxss10s01o0000o0s2x2000Q";
     public static final String UNLIMITED_LICENSE = "HazelcastEnterpriseHD#9999Nodes#RiUHNE1lOrwITY7F5K3VAm539qt3h099W999C99999xn999PsQ1Pv19q39h0";
     public static final String SECURITY_ONLY_LICENSE = "SecurityOnlyEnterprise#wO1FiI6V7RABrJfb03mcTk54xW210000G00hg10Z020Q129090Z4oqG0L994";
-    public static final String LICENSE_WITH_DIFFERENT_VERSION = "HazelcastEnterprise-3.6#2Nodes#OYFEaVrkfTbu3B6Uy7IwKc281e640D04212Dg40321014C000qP1X0Gq004M";
+    public static final String LICENSE_WITH_DIFFERENT_VERSION = "HazelcastEnterprise-3.6#10Nodes#i1FrKmSIRONz3T0AlkBYyU6790p19092d22X99p090Msg1G4nt13XM99699v";
     public static final String LICENSE_WITH_SMALLER_VERSION = "HazelcastEnterprise#2Nodes#2Clients#HDMemory:1024GB#OFN7iUaVTmjIB6SRArKc5bw319000240o011003021042q5Q0n1p0QLq30Wo";
     public static final String TWO_GB_V2_HD_LICENSE = "HazelcastEnterprise#10Nodes#HDMemory:2GB#fb7lyJ0AIFH6k5rjE1cwUBiS10000ZeQn909263p0s1q00d001Q190010M91";
     public static final String ENTERPRISE_HD_LICENSE = "HazelcastEnterpriseHD#2Nodes#maOHFiwR5YEcy1T6K7bJ0u290q21h9d19g00sX99C39399eG99Z9v0x9t9x0";

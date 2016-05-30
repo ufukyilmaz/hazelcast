322af84e2737aa23815cdc94313444e0a6f1fc6a
Ahmet MIRCIK <ahmetmircik@gmail.com>
2016-05-30 13:13:33 +0300

    Removes unnecessarily overridden test methods from HDEvictionTest, after introduction of sampling based eviction on os part, it is enough to extend EvictionTest for HDEvictionTest

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/HDEvictionTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDEvictionTest.java
@@ -17,82 +17,19 @@
 package com.hazelcast.map;
 
 import com.hazelcast.config.Config;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.MaxSizeConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
-import com.hazelcast.memory.StandardMemoryManager;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-
-import static com.hazelcast.config.EvictionPolicy.LFU;
-import static com.hazelcast.config.MaxSizeConfig.MaxSizePolicy.PER_NODE;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
 public class HDEvictionTest extends EvictionTest {
 
-    @BeforeClass
-    public static void setupClass() {
-        System.setProperty(StandardMemoryManager.PROPERTY_DEBUG_ENABLED, "true");
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-        System.setProperty(StandardMemoryManager.PROPERTY_DEBUG_ENABLED, "false");
-    }
-
     @Override
     protected Config getConfig() {
         return HDTestSupport.getHDConfig();
-    }
-
-
-    @Override
-    public void testEvictionLFU() {
-        testEvictionLFUInternal(false);
-    }
-
-    @Test
-    public void testEvictionLFU_statisticsDisabled() {
-        testEvictionLFUInternal(true);
-    }
-
-    /**
-     * This test is only testing occurrence of LFU eviction.
-     */
-    protected void testEvictionLFUInternal(boolean disableStats) {
-        int mapMaxSize = 10000;
-        String mapName = randomMapName();
-
-        MaxSizeConfig msc = new MaxSizeConfig();
-        msc.setMaxSizePolicy(PER_NODE);
-        msc.setSize(mapMaxSize);
-
-        Config config = getConfig();
-        MapConfig mapConfig = config.getMapConfig(mapName);
-        mapConfig.setStatisticsEnabled(disableStats);
-        mapConfig.setEvictionPolicy(LFU);
-        mapConfig.setMinEvictionCheckMillis(0);
-        mapConfig.setMaxSizeConfig(msc);
-
-        HazelcastInstance node = createHazelcastInstance(config);
-        IMap<Object, Object> map = node.getMap(mapName);
-
-        for (int i = 0; i < 2 * mapMaxSize; i++) {
-            map.put(i, i);
-        }
-
-        int mapSize = map.size();
-        assertTrue("Eviction did not work, map size " + mapSize + " should be smaller than allowed max size = " + mapMaxSize,
-                mapSize < mapMaxSize);
     }
 
 }

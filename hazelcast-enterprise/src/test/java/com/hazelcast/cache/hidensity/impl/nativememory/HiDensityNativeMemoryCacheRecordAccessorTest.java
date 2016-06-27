56211ee5b57a8e678076ad1cb5b0e9fdd0d1287c
Serkan OZAL <serkan@hazelcast.com>
2016-06-27 11:38:52 +0300

    Memory Access Abstraction Code Coverage Improvements

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/cache/hidensity/impl/nativememory/HiDensityNativeMemoryCacheRecordAccessorTest.java
@@ -0,0 +1,99 @@
+package com.hazelcast.cache.hidensity.impl.nativememory;
+
+import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
+import com.hazelcast.internal.serialization.impl.EnterpriseSerializationServiceBuilder;
+import com.hazelcast.internal.serialization.impl.NativeMemoryData;
+import com.hazelcast.memory.MemorySize;
+import com.hazelcast.memory.MemoryUnit;
+import com.hazelcast.memory.PoolingMemoryManager;
+import com.hazelcast.nio.serialization.DataType;
+import com.hazelcast.nio.serialization.EnterpriseSerializationService;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.internal.memory.MemoryAllocator.NULL_ADDRESS;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(EnterpriseSerialJUnitClassRunner.class)
+@Category(QuickTest.class)
+public class HiDensityNativeMemoryCacheRecordAccessorTest {
+
+    private PoolingMemoryManager memoryManager;
+    private EnterpriseSerializationService serializationService;
+    private HiDensityNativeMemoryCacheRecordAccessor accessor;
+
+    @Before
+    public void setup() {
+        MemorySize memorySize = new MemorySize(4, MemoryUnit.MEGABYTES);
+        memoryManager = new PoolingMemoryManager(memorySize);
+        memoryManager.registerThread(Thread.currentThread());
+        serializationService = new EnterpriseSerializationServiceBuilder().setMemoryManager(memoryManager).build();
+        accessor = new HiDensityNativeMemoryCacheRecordAccessor(serializationService, memoryManager);
+    }
+
+    @After
+    public void tearDown() {
+        memoryManager.dispose();
+    }
+
+    @Test
+    public void test_createRecord() {
+        assertTrue(accessor.createRecord() instanceof HiDensityNativeMemoryCacheRecord);
+    }
+
+    @Test
+    public void test_isEqual_equals_when_bothAddresses_are_same() {
+        assertTrue(accessor.isEqual(1234L, 1234L));
+    }
+
+    @Test
+    public void test_isEqual_equals_when_bothAddresses_are_null() {
+        assertTrue(accessor.isEqual(NULL_ADDRESS, NULL_ADDRESS));
+    }
+
+    @Test
+    public void test_isEqual_notEquals_when_onlyOneOfTheAddresses_is_null() {
+        assertFalse(accessor.isEqual(1234L, NULL_ADDRESS));
+        assertFalse(accessor.isEqual(NULL_ADDRESS, 1234L));
+    }
+
+    @Test
+    public void test_isEqual_equals_when_values_are_same() {
+        long recordAddress1 = memoryManager.allocate(HiDensityNativeMemoryCacheRecord.SIZE);
+        long recordAddress2 = memoryManager.allocate(HiDensityNativeMemoryCacheRecord.SIZE);
+
+        HiDensityNativeMemoryCacheRecord record1 = accessor.newRecord().reset(recordAddress1);
+        HiDensityNativeMemoryCacheRecord record2 = accessor.newRecord().reset(recordAddress2);
+
+        NativeMemoryData value1 = serializationService.toData(1, DataType.NATIVE);
+        NativeMemoryData value2 = serializationService.toData(1, DataType.NATIVE);
+
+        record1.setValue(value1);
+        record2.setValue(value2);
+
+        assertTrue(accessor.isEqual(record1.address(), record2.address()));
+    }
+
+    @Test
+    public void test_isEqual_notEquals_when_values_are_different() {
+        long recordAddress1 = memoryManager.allocate(HiDensityNativeMemoryCacheRecord.SIZE);
+        long recordAddress2 = memoryManager.allocate(HiDensityNativeMemoryCacheRecord.SIZE);
+
+        HiDensityNativeMemoryCacheRecord record1 = accessor.newRecord().reset(recordAddress1);
+        HiDensityNativeMemoryCacheRecord record2 = accessor.newRecord().reset(recordAddress2);
+
+        NativeMemoryData value1 = serializationService.toData(1, DataType.NATIVE);
+        NativeMemoryData value2 = serializationService.toData(2, DataType.NATIVE);
+
+        record1.setValue(value1);
+        record2.setValue(value2);
+
+        assertFalse(accessor.isEqual(record1.address(), record2.address()));
+    }
+
+}

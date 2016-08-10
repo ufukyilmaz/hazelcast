4e4b9ae7f13d5e0c654e4c03ff2d14f075beac00
Ahmet MIRCIK <ahmetmircik@gmail.com>
2016-08-10 11:30:23 +0300

    Renamed test class as HDPagingPredicateTest

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/query/HDPagingPredicateTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/query/HDPagingPredicateTest.java
@@ -19,7 +19,7 @@ package com.hazelcast.map.impl.query;
 import com.hazelcast.config.Config;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
 import com.hazelcast.map.HDTestSupport;
-import com.hazelcast.map.SortLimitTest;
+import com.hazelcast.map.PagingPredicateTest;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.experimental.categories.Category;
@@ -27,7 +27,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
-public class HDSortLimitTest extends SortLimitTest {
+public class HDPagingPredicateTest extends PagingPredicateTest {
 
     @Override
     protected Config getConfig() {

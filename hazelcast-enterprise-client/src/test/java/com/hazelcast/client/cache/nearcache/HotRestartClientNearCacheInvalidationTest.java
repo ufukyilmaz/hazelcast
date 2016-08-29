46a91146026e8645d868e413de375a7a06ba8b54
Vassilis Bekiaris <vbekiaris@gmail.com>
2016-08-29 07:17:52 +0300

    Introduces test to validate publishing of near-cache invalidation events

---
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/nearcache/HotRestartClientNearCacheInvalidationTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.client.cache.nearcache;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.CacheConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.HotRestartConfig;
+import com.hazelcast.config.HotRestartPersistenceConfig;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.NativeMemoryConfig;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.memory.MemorySize;
+import com.hazelcast.memory.MemoryUnit;
+import com.hazelcast.spi.properties.GroupProperty;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
+import java.io.File;
+
+import static com.hazelcast.nio.IOUtil.delete;
+import static com.hazelcast.nio.IOUtil.toFileName;
+
+/**
+ * Test publishing of near-cache invalidation events, when the cache is configured with NATIVE in-memory format and
+ * with hot restart enabled.
+ */
+public class HotRestartClientNearCacheInvalidationTest
+        extends ClientNearCacheInvalidationTest {
+
+    private static final MemorySize SERVER_NATIVE_MEMORY_SIZE = new MemorySize(64, MemoryUnit.MEGABYTES);
+    private static final MemorySize CLIENT_NATIVE_MEMORY_SIZE = new MemorySize(32, MemoryUnit.MEGABYTES);
+
+    @Rule
+    public TestName testName = new TestName();
+
+    private File folder;
+
+    @Override
+    public void setup() {
+        folder = new File(toFileName(getClass().getSimpleName()) + '_' + toFileName(testName.getMethodName()));
+        delete(folder);
+        if (!folder.mkdir() && !folder.exists()) {
+            throw new AssertionError("Unable to create test folder: " + folder.getAbsolutePath());
+        }
+        super.setup();
+    }
+
+    @Override
+    public void tearDown() {
+        super.tearDown();
+        if (folder != null) {
+            delete(folder);
+        }
+    }
+
+    @Override
+    protected Config getConfig() {
+        Config cfg = new Config();
+        NativeMemoryConfig nativeMemoryConfig =
+                new NativeMemoryConfig()
+                        .setSize(SERVER_NATIVE_MEMORY_SIZE)
+                        .setEnabled(true);
+        cfg.setNativeMemoryConfig(nativeMemoryConfig);
+        cfg.setProperty(GroupProperty.PARTITION_OPERATION_THREAD_COUNT.getName(), "4");
+        HotRestartPersistenceConfig hotRestartPersistenceConfig = cfg.getHotRestartPersistenceConfig();
+        hotRestartPersistenceConfig.setEnabled(true);
+        hotRestartPersistenceConfig.setBaseDir(folder);
+        return cfg;
+    }
+
+    @Override
+    protected ClientConfig createClientConfig() {
+        ClientConfig clientConfig = super.createClientConfig();
+        clientConfig.setNativeMemoryConfig(new NativeMemoryConfig().setSize(CLIENT_NATIVE_MEMORY_SIZE).setEnabled(true));
+        return clientConfig;
+    }
+
+    @Override
+    protected NearCacheConfig createNearCacheConfig(InMemoryFormat inMemoryFormat) {
+        return super.createNearCacheConfig(inMemoryFormat);
+    }
+
+    @Override
+    protected CacheConfig createCacheConfig(InMemoryFormat inMemoryFormat) {
+        CacheConfig cacheConfig = super.createCacheConfig(InMemoryFormat.NATIVE);
+        EvictionConfig evictionConfig = new EvictionConfig();
+        evictionConfig.setMaximumSizePolicy(EvictionConfig.MaxSizePolicy.USED_NATIVE_MEMORY_PERCENTAGE);
+        evictionConfig.setSize(99);
+        cacheConfig.setEvictionConfig(evictionConfig);
+        // enable hot restart
+        HotRestartConfig hrConfig = new HotRestartConfig().setEnabled(true);
+        cacheConfig.setHotRestartConfig(hrConfig);
+
+        return cacheConfig;
+    }
+}

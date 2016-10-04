2b99bd053223d9c900644b90331159b9dd2c544c
donnerbart <github@thunderphreak.de>
2016-10-04 13:27:53 +0200

    Added method getICache() to HDTestSupport to retrieve an ICache without unchecked warnings

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/HDTestSupport.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDTestSupport.java
@@ -16,6 +16,8 @@
 
 package com.hazelcast.map;
 
+import com.hazelcast.cache.ICache;
+import com.hazelcast.config.CacheConfiguration;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.MapConfig;
@@ -24,6 +26,8 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IEnterpriseMap;
 import com.hazelcast.memory.MemorySize;
 import com.hazelcast.memory.MemoryUnit;
+
+import javax.cache.CacheManager;
 
 /**
  * Support class to provide hd specific configuration for map tests
@@ -53,5 +57,9 @@ public final class HDTestSupport {
 
     public static <K, V> IEnterpriseMap<K, V> getEnterpriseMap(HazelcastInstance instance, String mapName) {
         return (IEnterpriseMap<K, V>) instance.<K, V>getMap(mapName);
+    }
+
+    public static <K, V> ICache<K, V> getICache(CacheManager manager, CacheConfiguration<K, V> config, String cacheName) {
+        return (ICache<K, V>) manager.createCache(cacheName, config);
     }
 }

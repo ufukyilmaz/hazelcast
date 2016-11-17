c3bc5c9b909be980d731431c82f1239fd6424604
Mehmet Dogan <mehmet@dogan.io>
2016-11-17 13:26:57 +0300

    Use mock Node instead of starting member in MemberListReaderWriterTest

--- hazelcast-enterprise/src/test/java/com/hazelcast/spi/hotrestart/cluster/MemberListReaderWriterTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/spi/hotrestart/cluster/MemberListReaderWriterTest.java
@@ -1,14 +1,12 @@
 package com.hazelcast.spi.hotrestart.cluster;
 
-import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.Member;
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
 import com.hazelcast.instance.MemberImpl;
+import com.hazelcast.instance.Node;
 import com.hazelcast.nio.Address;
-import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
-import com.hazelcast.util.UuidUtil;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -19,30 +17,25 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 
-import static com.hazelcast.test.HazelcastTestSupport.getNode;
+import static com.hazelcast.util.UuidUtil.newUnsecureUuidString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(EnterpriseSerialJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
 public class MemberListReaderWriterTest extends MetadataReaderWriterTestBase {
 
-    private TestHazelcastInstanceFactory factory;
-
-    private HazelcastInstance instance;
+    private Node node = mock(Node.class);
 
     @Override
     void setupInternal() {
-        factory = new TestHazelcastInstanceFactory(1);
-        instance = factory.newHazelcastInstance();
-    }
-
-    @Override
-    void tearDownInternal() {
-        factory.terminateAll();
-        factory = null;
+        Address address = new Address("127.0.0.1", localAddress, 5701);
+        MemberImpl localMember = new MemberImpl(address, true, newUnsecureUuidString(), null);
+        when(node.getLocalMember()).thenReturn(localMember);
     }
 
     @Test
@@ -65,32 +58,32 @@ public class MemberListReaderWriterTest extends MetadataReaderWriterTestBase {
 
     @Test(expected = FileNotFoundException.class)
     public void test_writeNotExistingFolder() throws IOException {
-        MemberListWriter writer = new MemberListWriter(getNonExistingFolder(), getNode(instance));
+        MemberListWriter writer = new MemberListWriter(getNonExistingFolder(), node);
         writer.write(Collections.<Member>emptyList());
     }
 
     @Test
     public void test_EmptyWriteRead() throws IOException {
-        MemberListWriter writer = new MemberListWriter(folder, getNode(instance));
+        MemberListWriter writer = new MemberListWriter(folder, node);
         writer.write(Collections.<Member>emptyList());
 
         MemberListReader reader = new MemberListReader(folder);
         reader.read();
 
-        assertEquals(getNode(instance).getLocalMember(), reader.getLocalMember());
+        assertEquals(node.getLocalMember(), reader.getLocalMember());
         assertTrue(reader.getMembers().isEmpty());
     }
 
     @Test
     public void test_WriteRead() throws IOException {
         Collection<Member> members = initializeMembers(100);
-        MemberListWriter writer = new MemberListWriter(folder, getNode(instance));
+        MemberListWriter writer = new MemberListWriter(folder, node);
         writer.write(members);
 
         MemberListReader reader = new MemberListReader(folder);
         reader.read();
 
-        assertEquals(getNode(instance).getLocalMember(), reader.getLocalMember());
+        assertEquals(node.getLocalMember(), reader.getLocalMember());
 
         Collection<MemberImpl> readMembers = reader.getMembers();
         assertNotNull(readMembers);
@@ -102,9 +95,9 @@ public class MemberListReaderWriterTest extends MetadataReaderWriterTestBase {
         Address[] addresses = initializeAddresses(memberCount - 1);
         Collection<Member> members = new HashSet<Member>(memberCount);
         for (Address address : addresses) {
-            members.add(new MemberImpl(address, false, UuidUtil.newUnsecureUuidString(), null));
+            members.add(new MemberImpl(address, false, newUnsecureUuidString(), null));
         }
-        members.add(new MemberImpl(getNode(instance).getLocalMember()));
+        members.add(new MemberImpl(node.getLocalMember()));
         return members;
     }
 }

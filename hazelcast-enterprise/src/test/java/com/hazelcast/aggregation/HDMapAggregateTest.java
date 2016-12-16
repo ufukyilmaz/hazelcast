a3fa139ddc6958a86d8b320ffc69e59c14bdfae0
Tomek Bujok <tom.bujok@gmail.com>
2016-12-16 12:33:47 +0100

    Fast-aggregations and Projections test coverage for Native memory (#1208)

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/aggregation/HDMapAggregateTest.java
@@ -0,0 +1,32 @@
+package com.hazelcast.aggregation;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NativeMemoryConfig;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.map.HDTestSupport.NATIVE_MEMORY_SIZE;
+
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class HDMapAggregateTest extends MapAggregateTest {
+
+    @Override
+    public void doWithConfig(Config config) {
+        MapConfig mapConfig = config.getMapConfig("aggr");
+        mapConfig.setInMemoryFormat(InMemoryFormat.NATIVE);
+
+        NativeMemoryConfig memoryConfig = new NativeMemoryConfig()
+                .setEnabled(true)
+                .setSize(NATIVE_MEMORY_SIZE)
+                .setAllocatorType(NativeMemoryConfig.MemoryAllocatorType.STANDARD);
+
+        config.setNativeMemoryConfig(memoryConfig);
+    }
+
+}

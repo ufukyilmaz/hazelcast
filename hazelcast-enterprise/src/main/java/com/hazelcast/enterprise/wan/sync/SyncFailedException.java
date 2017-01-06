e9f164873ffa9ab675526e45063da7ae0b199d2c
donnerbart <github@thunderphreak.de>
2017-01-06 10:57:18 +0100

    Increase code coverage of SyncFailedException

--- hazelcast-enterprise/src/main/java/com/hazelcast/enterprise/wan/sync/SyncFailedException.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/enterprise/wan/sync/SyncFailedException.java
@@ -1,15 +1,11 @@
 package com.hazelcast.enterprise.wan.sync;
 
 /**
- * A SyncFailedException is thrown when a WAN Sync operation is failed due to any reason
+ * A SyncFailedException is thrown when a WAN Sync operation is failed due to any reason.
  */
 public class SyncFailedException extends RuntimeException {
 
-    public SyncFailedException(String s) {
+    SyncFailedException(String s) {
         super(s);
-    }
-
-    public SyncFailedException(String s, Throwable throwable) {
-        super(s, throwable);
     }
 }

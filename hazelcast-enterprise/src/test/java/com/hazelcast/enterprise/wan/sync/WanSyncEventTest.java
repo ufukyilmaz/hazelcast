9449507bbd09ef32b132976261d1b2c512b806db
donnerbart <github@thunderphreak.de>
2017-01-10 23:46:09 +0100

    Increased code coverage of WanSyncEvent.

--- hazelcast-enterprise/src/test/java/com/hazelcast/enterprise/wan/sync/WanSyncEventTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/enterprise/wan/sync/WanSyncEventTest.java
@@ -1,12 +1,21 @@
 package com.hazelcast.enterprise.wan.sync;
 
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.internal.serialization.impl.EnterpriseSerializationServiceBuilder;
+import com.hazelcast.nio.serialization.Data;
+import com.hazelcast.nio.serialization.EnterpriseSerializationService;
 import com.hazelcast.test.RequireAssertEnabled;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
@@ -19,7 +28,33 @@ public class WanSyncEventTest {
 
     @Test(expected = AssertionError.class)
     @RequireAssertEnabled
-    public void testConstructor_whenTypeIsNotAllMaps_thenAssert() throws Exception {
+    public void testConstructor_whenTypeIsNull_thenAssert() {
+        new WanSyncEvent(null);
+    }
+
+    @Test(expected = AssertionError.class)
+    @RequireAssertEnabled
+    public void testConstructor_whenTypeIsNotAllMaps_thenAssert() {
         new WanSyncEvent(WanSyncType.SINGLE_MAP);
+    }
+
+    @Test
+    public void testSerialization() {
+        Set<Integer> partitionIds = new HashSet<Integer>();
+        partitionIds.add(23);
+        partitionIds.add(42);
+
+        WanSyncEvent expected = new WanSyncEvent(WanSyncType.ALL_MAPS, "eventName");
+        expected.setPartitionSet(partitionIds);
+        expected.setOp(new WanSyncOperation());
+
+        EnterpriseSerializationService serializationService = new EnterpriseSerializationServiceBuilder().build();
+        Data serialized = serializationService.toData(expected);
+        WanSyncEvent deserialized = serializationService.toObject(serialized, WanSyncEvent.class);
+
+        assertEquals(expected.getType(), deserialized.getType());
+        assertEquals(expected.getName(), deserialized.getName());
+        assertEquals(expected.getPartitionSet(), deserialized.getPartitionSet());
+        assertNull(deserialized.getOp());
     }
 }

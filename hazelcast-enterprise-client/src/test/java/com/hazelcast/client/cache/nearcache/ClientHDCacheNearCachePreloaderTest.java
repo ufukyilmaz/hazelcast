de77f4c3043d8892f293b5537df21e3874958481
donnerbart <github@thunderphreak.de>
2017-01-27 19:02:33 +0100

    Adapted Near Cache Preloader tests to OS changes
    * Fixed missing resource files
    * Removed InMemoryFormat from @Parameters
    * Added storeAndLoad() tests with InMemoryFormat.NATIVE

--- hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/nearcache/ClientHDCacheNearCachePreloaderTest.java
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/nearcache/ClientHDCacheNearCachePreloaderTest.java
@@ -4,6 +4,9 @@ import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.enterprise.SampleLicense;
+import com.hazelcast.test.annotation.SlowTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
@@ -14,17 +17,25 @@ import static com.hazelcast.cache.nearcache.HiDensityNearCacheTestUtils.getHDCon
 
 public class ClientHDCacheNearCachePreloaderTest extends ClientCacheNearCachePreloaderTest {
 
-    @Parameters(name = "format:{0} invalidationOnChange:{1}")
+    @Parameters(name = "invalidationOnChange:{0}")
     public static Collection<Object[]> parameters() {
         // FIXME: the Near Cache pre-loader doesn't work with enabled invalidations due to a known getAll() issue!
         return Arrays.asList(new Object[][]{
-                {InMemoryFormat.BINARY, false},
-                //{InMemoryFormat.BINARY, true},
-                {InMemoryFormat.OBJECT, false},
-                //{InMemoryFormat.OBJECT, true},
-                {InMemoryFormat.NATIVE, false},
-                //{InMemoryFormat.NATIVE, true},
+                {false},
+                //{true},
         });
+    }
+
+    @Test(timeout = TEST_TIMEOUT)
+    @Category(SlowTest.class)
+    public void testStoreAndLoad_withIntegerKeys_withInMemoryFormatNative() {
+        storeAndLoad(2342, false, InMemoryFormat.NATIVE);
+    }
+
+    @Test(timeout = TEST_TIMEOUT)
+    @Category(SlowTest.class)
+    public void testStoreAndLoad_withStringKeys_withInMemoryFormatNative() {
+        storeAndLoad(4223, true, InMemoryFormat.NATIVE);
     }
 
     @Override

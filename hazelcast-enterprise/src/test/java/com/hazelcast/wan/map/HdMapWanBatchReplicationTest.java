5555cf1320880a80838e84d819b250c535d3d318
Matko Medenjak <matko@hazelcast.com>
2017-03-20 17:04:37 +0100

    Set enterprise license for wan tests
    The test was using the Hazelcast JUnit runner which doesn't set the license. The fix changes the runner to the EE one. Also remove unused imports.
    
    Fixes: https://github.com/hazelcast/hazelcast-enterprise/issues/1350

--- hazelcast-enterprise/src/test/java/com/hazelcast/wan/map/HdMapWanBatchReplicationTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/wan/map/HdMapWanBatchReplicationTest.java
@@ -2,10 +2,8 @@ package com.hazelcast.wan.map;
 
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.enterprise.wan.replication.WanBatchReplication;
-import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.SlowTest;
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
 
 @Category(SlowTest.class)
 public class HdMapWanBatchReplicationTest extends AbstractMapWanReplicationTest {

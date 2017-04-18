900b58754333cbf52da3cca945fe96e82f4ab91b
Ensar Basri Kahveci <ebkahveci@gmail.com>
2017-04-18 10:13:43 +0300

    Convert map service into FragmentedMigrationAwareService
    * Implement ReplicaFragmentAware in MapOperation

--- hazelcast-enterprise/src/main/java/com/hazelcast/map/impl/EnterpriseMapMigrationAwareService.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/map/impl/EnterpriseMapMigrationAwareService.java
@@ -3,6 +3,9 @@ package com.hazelcast.map.impl;
 import com.hazelcast.map.impl.operation.EnterpriseMapReplicationOperation;
 import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.PartitionReplicationEvent;
+import com.hazelcast.spi.ServiceNamespace;
+
+import java.util.Collection;
 
 /**
  * Defines enterprise only migration aware service behavior for {@link MapService}.
@@ -23,8 +26,19 @@ public class EnterpriseMapMigrationAwareService extends MapMigrationAwareService
         int partitionId = event.getPartitionId();
         PartitionContainer container = mapServiceContext.getPartitionContainer(partitionId);
 
-        EnterpriseMapReplicationOperation operation
-                = new EnterpriseMapReplicationOperation(container, partitionId, event.getReplicaIndex());
+        Operation operation = new EnterpriseMapReplicationOperation(container, partitionId, event.getReplicaIndex());
+        operation.setService(mapServiceContext.getService());
+
+        return operation;
+    }
+
+    @Override
+    public Operation prepareReplicationOperation(PartitionReplicationEvent event,
+                                                 Collection<ServiceNamespace> namespaces) {
+        int partitionId = event.getPartitionId();
+        PartitionContainer container = mapServiceContext.getPartitionContainer(partitionId);
+
+        Operation operation = new EnterpriseMapReplicationOperation(container, namespaces, partitionId, event.getReplicaIndex());
         operation.setService(mapServiceContext.getService());
 
         return operation;

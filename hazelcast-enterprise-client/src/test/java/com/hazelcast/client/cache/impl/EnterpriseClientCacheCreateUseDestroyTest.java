5794acaf265bbde6c9c14ea29c349e6582ea6dd5
donnerbart <github@thunderphreak.de>
2017-08-23 20:22:22 +0200

    Ignored EnterpriseClientCacheCreateUseDestroyTest

--- hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/impl/EnterpriseClientCacheCreateUseDestroyTest.java
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/impl/EnterpriseClientCacheCreateUseDestroyTest.java
@@ -1,9 +1,11 @@
 package com.hazelcast.client.cache.impl;
 
 import com.hazelcast.enterprise.EnterpriseParametersRunnerFactory;
+import org.junit.Ignore;
 import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 @UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
+@Ignore(value = "https://github.com/hazelcast/hazelcast-enterprise/issues/1653")
 public class EnterpriseClientCacheCreateUseDestroyTest extends ClientCacheCreateUseDestroyTest {
 
     @Override

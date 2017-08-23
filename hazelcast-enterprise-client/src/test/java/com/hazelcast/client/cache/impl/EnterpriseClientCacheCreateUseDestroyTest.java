735ba24f944ce6dcd82c7a6253c6bb943c40bef5
donnerbart <github@thunderphreak.de>
2017-08-23 18:12:37 +0200

    Removed @Ignore from EnterpriseClientCacheCreateUseDestroyTest

--- hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/impl/EnterpriseClientCacheCreateUseDestroyTest.java
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/impl/EnterpriseClientCacheCreateUseDestroyTest.java
@@ -1,10 +1,8 @@
 package com.hazelcast.client.cache.impl;
 
 import com.hazelcast.enterprise.EnterpriseParametersRunnerFactory;
-import org.junit.Ignore;
 import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
-@Ignore
 @UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
 public class EnterpriseClientCacheCreateUseDestroyTest extends ClientCacheCreateUseDestroyTest {
 
--- hazelcast-enterprise/src/test/java/com/hazelcast/cache/impl/EnterpriseCacheCreateUseDestroyTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/cache/impl/EnterpriseCacheCreateUseDestroyTest.java
@@ -1,9 +1,9 @@
 package com.hazelcast.cache.impl;
 
 import com.hazelcast.enterprise.EnterpriseParametersRunnerFactory;
-import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
-@Parameterized.UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
+@UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
 public class EnterpriseCacheCreateUseDestroyTest extends CacheCreateUseDestroyTest {
 
     @Override

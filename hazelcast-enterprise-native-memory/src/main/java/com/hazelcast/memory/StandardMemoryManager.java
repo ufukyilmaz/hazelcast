3bb9131ed041e333bad1f938f021819106857ea5
donnerbart <github@thunderphreak.de>
2017-08-30 19:59:23 +0200

    Added stack trace debugging to StandardMemoryManager
    * added debug property to enable stack trace debugging in the
      StandardMemoryManager
    * added static boolean field to NativeMemoryTestUtil to enable
      stack trace debugging
    * added stack trace grouped output of memory allocations to
      NativeMemoryTestUtil

--- hazelcast-enterprise-native-memory/src/main/java/com/hazelcast/memory/StandardMemoryManager.java
+++ hazelcast-enterprise-native-memory/src/main/java/com/hazelcast/memory/StandardMemoryManager.java
@@ -9,6 +9,7 @@ import com.hazelcast.internal.util.counters.Counter;
 import com.hazelcast.internal.util.counters.MwCounter;
 import com.hazelcast.util.ExceptionUtil;
 import com.hazelcast.util.collection.Long2LongHashMap;
+import com.hazelcast.util.collection.Long2ObjectHashMap;
 import com.hazelcast.util.function.LongLongConsumer;
 
 import static com.hazelcast.internal.memory.GlobalMemoryAccessorRegistry.AMEM;
@@ -16,46 +17,82 @@ import static com.hazelcast.internal.memory.GlobalMemoryAccessorRegistry.AMEM;
 public final class StandardMemoryManager implements HazelcastMemoryManager, MetricsProvider {
 
     /**
-     * System property to enable debug mode of {@link StandardMemoryManager}.
+     * System property to enable the debug mode of {@link StandardMemoryManager}.
      */
     public static final String PROPERTY_DEBUG_ENABLED = "hazelcast.memory.manager.debug.enabled";
+
+    /**
+     * System property to enable the debug mode with stacktraces of {@link StandardMemoryManager}.
+     */
+    public static final String PROPERTY_DEBUG_STACKTRACE_ENABLED = "hazelcast.memory.manager.debug.stacktrace.enabled";
+
+    /**
+     * Defines how many stacktrace elements are skipped, to hide internal method calls from user.
+     */
+    private static final int STACK_TRACE_OFFSET = 3;
+
     private static final FreeMemoryChecker DEFAULT_FREE_MEMORY_CHECKER = new FreeMemoryChecker();
 
+    private final Counter sequenceGenerator = MwCounter.newMwCounter();
+
+    private final boolean isDebugStackTraceEnabled;
     private final boolean isDebugEnabled;
 
     private final LibMalloc malloc;
     private final NativeMemoryStats memoryStats;
-    private final Counter sequenceGenerator = MwCounter.newMwCounter();
 
     private final Long2LongHashMap allocatedBlocks;
+    private final Long2ObjectHashMap<String> allocatedStackTraces;
+    private final StringBuilder stackTraceStringBuilder;
 
     public StandardMemoryManager(MemorySize cap) {
         this(cap, DEFAULT_FREE_MEMORY_CHECKER);
     }
 
     public StandardMemoryManager(MemorySize cap, FreeMemoryChecker freeMemoryChecker) {
-        isDebugEnabled = Boolean.getBoolean(PROPERTY_DEBUG_ENABLED);
+        this.isDebugStackTraceEnabled = Boolean.getBoolean(PROPERTY_DEBUG_STACKTRACE_ENABLED);
+        this.isDebugEnabled = isDebugStackTraceEnabled || Boolean.getBoolean(PROPERTY_DEBUG_ENABLED);
 
         long size = cap.bytes();
         freeMemoryChecker.checkFreeMemory(size);
-        malloc = new UnsafeMalloc();
-        memoryStats = new NativeMemoryStats(size);
 
-        allocatedBlocks = initAllocatedBlocks();
+        this.malloc = new UnsafeMalloc();
+        this.memoryStats = new NativeMemoryStats(size);
+
+        this.allocatedBlocks = initAllocatedBlocks();
+        this.allocatedStackTraces = initAllocatedStacktraces();
+        this.stackTraceStringBuilder = initStackTraceStringBuilder();
     }
 
     StandardMemoryManager(LibMalloc malloc, NativeMemoryStats memoryStats) {
-        isDebugEnabled = Boolean.getBoolean(PROPERTY_DEBUG_ENABLED);
+        this.isDebugStackTraceEnabled = Boolean.getBoolean(PROPERTY_DEBUG_STACKTRACE_ENABLED);
+        this.isDebugEnabled = isDebugStackTraceEnabled || Boolean.getBoolean(PROPERTY_DEBUG_ENABLED);
 
         this.malloc = malloc;
         this.memoryStats = memoryStats;
 
-        allocatedBlocks = initAllocatedBlocks();
+        this.allocatedBlocks = initAllocatedBlocks();
+        this.allocatedStackTraces = initAllocatedStacktraces();
+        this.stackTraceStringBuilder = initStackTraceStringBuilder();
     }
 
     private Long2LongHashMap initAllocatedBlocks() {
         if (isDebugEnabled) {
             return new Long2LongHashMap(NULL_ADDRESS);
+        }
+        return null;
+    }
+
+    private Long2ObjectHashMap<String> initAllocatedStacktraces() {
+        if (isDebugStackTraceEnabled) {
+            return new Long2ObjectHashMap<String>();
+        }
+        return null;
+    }
+
+    private StringBuilder initStackTraceStringBuilder() {
+        if (isDebugStackTraceEnabled) {
+            return new StringBuilder();
         }
         return null;
     }
@@ -121,20 +158,6 @@ public final class StandardMemoryManager implements HazelcastMemoryManager, Metr
         }
     }
 
-    protected static void checkNotNull(long address, long size) {
-        if (address == NULL_ADDRESS) {
-            throw new NativeOutOfMemoryError("Not enough contiguous memory available!"
-                    + " Cannot acquire " + MemorySize.toPrettyString(size) + "!");
-        }
-    }
-
-    private synchronized void traceAllocation(long address, long size) {
-        long current = allocatedBlocks.put(address, size);
-        if (current != NULL_ADDRESS) {
-            throw new AssertionError("Already allocated! " + address);
-        }
-    }
-
     @Override
     public void free(long address, long size) {
         assert address != NULL_ADDRESS : "Invalid address: " + address + ", size: " + size;
@@ -146,18 +169,6 @@ public final class StandardMemoryManager implements HazelcastMemoryManager, Metr
 
         malloc.free(address);
         memoryStats.removeCommittedNative(size);
-    }
-
-    private synchronized void traceRelease(long address, long size) {
-        long current = allocatedBlocks.remove(address);
-        if (current != size) {
-            if (current == NULL_ADDRESS) {
-                throw new AssertionError("Either not allocated or duplicate free()! "
-                        + "Address: " + address + ", Size: " + size);
-            }
-            throw new AssertionError("Invalid size! Address: " + address
-                    + ", Expected: " + current + ", Actual: " + size);
-        }
     }
 
     @Override
@@ -201,14 +212,6 @@ public final class StandardMemoryManager implements HazelcastMemoryManager, Metr
         return SIZE_INVALID;
     }
 
-    public synchronized void forEachAllocatedBlock(LongLongConsumer consumer) {
-        if (isDebugEnabled) {
-            allocatedBlocks.longForEach(consumer);
-            return;
-        }
-        throw new UnsupportedOperationException("Allocated blocks are tracked only in DEBUG mode!");
-    }
-
     @Override
     public long newSequence() {
         return sequenceGenerator.inc();
@@ -217,5 +220,64 @@ public final class StandardMemoryManager implements HazelcastMemoryManager, Metr
     @Override
     public void provideMetrics(MetricsRegistry registry) {
         registry.scanAndRegister(memoryStats, "memorymanager.stats");
+    }
+
+    public synchronized void forEachAllocatedBlock(LongLongConsumer consumer) {
+        if (!isDebugEnabled) {
+            throw new UnsupportedOperationException("Allocated blocks are tracked only in DEBUG mode!");
+        }
+        allocatedBlocks.longForEach(consumer);
+    }
+
+    public synchronized Long2ObjectHashMap<String> getAllocatedStackTraces() {
+        if (!isDebugStackTraceEnabled) {
+            throw new UnsupportedOperationException("Allocated blocks are tracked only in DEBUG_STACKTRACE mode!");
+        }
+        return allocatedStackTraces;
+    }
+
+    private synchronized void traceAllocation(long address, long size) {
+        long current = allocatedBlocks.put(address, size);
+        if (current != NULL_ADDRESS) {
+            throw new AssertionError("Already allocated! " + address);
+        }
+        if (isDebugStackTraceEnabled) {
+            allocatedStackTraces.put(address, getStackTrace());
+        }
+    }
+
+    private synchronized void traceRelease(long address, long size) {
+        long current = allocatedBlocks.remove(address);
+        if (current != size) {
+            if (current == NULL_ADDRESS) {
+                throw new AssertionError("Either not allocated or duplicate free()! "
+                        + "Address: " + address + ", Size: " + size);
+            }
+            throw new AssertionError("Invalid size! Address: " + address
+                    + ", Expected: " + current + ", Actual: " + size);
+        }
+        if (isDebugStackTraceEnabled) {
+            allocatedStackTraces.remove(address);
+        }
+    }
+
+    private String getStackTrace() {
+        String prefix = "";
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (int i = STACK_TRACE_OFFSET; i < stackTraceElements.length; i++) {
+            stackTraceStringBuilder.append(prefix).append(stackTraceElements[i].toString());
+            prefix = "\n\t";
+        }
+
+        String stackTrace = stackTraceStringBuilder.toString();
+        stackTraceStringBuilder.setLength(0);
+        return stackTrace;
+    }
+
+    protected static void checkNotNull(long address, long size) {
+        if (address == NULL_ADDRESS) {
+            throw new NativeOutOfMemoryError("Not enough contiguous memory available!"
+                    + " Cannot acquire " + MemorySize.toPrettyString(size) + "!");
+        }
     }
 }

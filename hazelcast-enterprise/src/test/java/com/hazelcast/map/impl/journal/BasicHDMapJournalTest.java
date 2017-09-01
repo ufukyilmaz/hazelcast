8afdfd1a4863d55f1b92a412597ab5acb335127c
Matko Medenjak <matko@hazelcast.com>
2017-09-01 15:50:17 +0200

    Increase test journal capacity
    The capacity of the event journal should be increased because it is
    now shared between parititions.
    See : https://github.com/hazelcast/hazelcast/pull/11257
    
    Fixes : https://github.com/hazelcast/hazelcast-enterprise/issues/1678

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/journal/BasicHDMapJournalTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/journal/BasicHDMapJournalTest.java
@@ -4,19 +4,21 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.EventJournalConfig;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
-import org.junit.Ignore;
+import com.hazelcast.spi.properties.GroupProperty;
 import org.junit.runner.RunWith;
 
 import static com.hazelcast.HDTestSupport.getHDConfig;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
-@Ignore(value = "https://github.com/hazelcast/hazelcast-enterprise/issues/1678")
 public class BasicHDMapJournalTest extends BasicMapJournalTest {
 
     @Override
     protected Config getConfig() {
         final Config config = getHDConfig();
-        config.addEventJournalConfig(new EventJournalConfig().setMapName("default").setEnabled(true));
+        final int defaultPartitionCount = Integer.parseInt(GroupProperty.PARTITION_COUNT.getDefaultValue());
+        config.addEventJournalConfig(new EventJournalConfig().setMapName("default")
+                                                             .setCapacity(200 * defaultPartitionCount)
+                                                             .setEnabled(true));
         final MapConfig mapConfig = new MapConfig("mappy");
         final MapConfig expiringMap = new MapConfig("expiring").setTimeToLiveSeconds(1);
         return config.addMapConfig(mapConfig).addMapConfig(expiringMap);

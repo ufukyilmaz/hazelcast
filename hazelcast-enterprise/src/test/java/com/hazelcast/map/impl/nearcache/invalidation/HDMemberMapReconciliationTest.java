f2fb68dd8cedc51bfa726a53144686de3c1d5613
Ahmet Mırçık <ahmetmircik@gmail.com>
2017-09-12 11:26:41 +0300

    Added HDMemberMapReconciliationTest

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/invalidation/HDMemberMapReconciliationTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.map.impl.nearcache.invalidation;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.enterprise.EnterpriseParametersRunnerFactory;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+import static com.hazelcast.config.InMemoryFormat.BINARY;
+import static com.hazelcast.config.InMemoryFormat.NATIVE;
+import static com.hazelcast.config.InMemoryFormat.OBJECT;
+import static java.util.Arrays.asList;
+
+@RunWith(Parameterized.class)
+@Parameterized.UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class HDMemberMapReconciliationTest extends MemberMapReconciliationTest {
+
+    @Parameterized.Parameters(name = "mapInMemoryFormat:{0} nearCacheInMemoryFormat:{1}")
+    public static Collection<Object[]> parameters() {
+        return asList(new Object[][]{
+                {BINARY, BINARY},
+                {BINARY, OBJECT},
+                {BINARY, NATIVE},
+                {OBJECT, BINARY},
+                {OBJECT, OBJECT},
+                {OBJECT, NATIVE},
+                {NATIVE, BINARY},
+                {NATIVE, OBJECT},
+                {NATIVE, NATIVE},
+        });
+    }
+
+
+    @Override
+    protected Config getConfig() {
+        return getHDConfig();
+    }
+
+}

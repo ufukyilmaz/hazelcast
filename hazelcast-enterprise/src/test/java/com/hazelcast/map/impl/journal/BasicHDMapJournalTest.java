89c01122c300e1244d0e8725ffeec716bbe6c413
donnerbart <github@thunderphreak.de>
2017-09-19 13:21:46 +0200

    Minimal cleanup of BasicHDMapJournalTest

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/journal/BasicHDMapJournalTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/journal/BasicHDMapJournalTest.java
@@ -14,13 +14,20 @@ public class BasicHDMapJournalTest extends BasicMapJournalTest {
 
     @Override
     protected Config getConfig() {
-        final Config config = getHDConfig();
-        final int defaultPartitionCount = Integer.parseInt(GroupProperty.PARTITION_COUNT.getDefaultValue());
-        config.addEventJournalConfig(new EventJournalConfig().setMapName("default")
-                                                             .setCapacity(200 * defaultPartitionCount)
-                                                             .setEnabled(true));
-        final MapConfig mapConfig = new MapConfig("mappy");
-        final MapConfig expiringMap = new MapConfig("expiring").setTimeToLiveSeconds(1);
-        return config.addMapConfig(mapConfig).addMapConfig(expiringMap);
+        int defaultPartitionCount = Integer.parseInt(GroupProperty.PARTITION_COUNT.getDefaultValue());
+        EventJournalConfig eventJournalConfig = new EventJournalConfig()
+                .setEnabled(true)
+                .setMapName("default")
+                .setCapacity(200 * defaultPartitionCount);
+
+        MapConfig mapConfig = new MapConfig("mappy");
+
+        MapConfig expiringMap = new MapConfig("expiring")
+                .setTimeToLiveSeconds(1);
+
+        return getHDConfig()
+                .addEventJournalConfig(eventJournalConfig)
+                .addMapConfig(mapConfig)
+                .addMapConfig(expiringMap);
     }
 }

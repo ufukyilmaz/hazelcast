7615d13b4b9177340398d935fbf2002ad8e2b9a9
Matko Medenjak <matko@hazelcast.com>
2017-09-26 15:06:08 +0200

    Skip disposing deferred blocks in case of partition agnostic operation
    All operations extending HDMapOperation dispose deferred blocks on
    error and as such are expected to be partition operations. This is
    not always the case. For instance, the HDQueryOperation is run on a
    generic thread. The fix skips disposing deferred blocks in case when
    the operation is run on a generic thread.
    
    Fixes: https://github.com/hazelcast/hazelcast-enterprise/issues/1735
    
    (cherry picked from commit 97c8ff0)

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/HDBasicMapTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDBasicMapTest.java
@@ -1,12 +1,17 @@
 package com.hazelcast.map;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.instance.Node;
+import com.hazelcast.map.impl.MapService;
+import com.hazelcast.map.impl.operation.HDMapOperation;
 import com.hazelcast.memory.StandardMemoryManager;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
@@ -30,5 +35,35 @@ public class HDBasicMapTest extends BasicMapTest {
     @Override
     protected Config getConfig() {
         return HDTestSupport.getHDConfig();
+    }
+
+
+    /**
+     * Asserts that the exception handling logic for a HD map operation does not try to load the record store
+     * when the operation is not run on a partition thread.
+     * See: https://github.com/hazelcast/hazelcast-enterprise/issues/1735
+     */
+    @Test
+    public void HDMapOperationRunOnGenericThreadDoesNotDisposeDeferredBlocks() {
+        final HazelcastInstance instance = getInstance();
+        final Node node = getNode(instance);
+        node.nodeEngine.getOperationService().invokeOnTarget(MapService.SERVICE_NAME,
+                new ExceptionThrowingOp("mappy"), node.getThisAddress());
+    }
+
+    public static class ExceptionThrowingOp extends HDMapOperation {
+        public ExceptionThrowingOp(String mapName) {
+            super(mapName);
+        }
+
+        @Override
+        public int getId() {
+            return 0;
+        }
+
+        @Override
+        protected void runInternal() {
+            throw new RuntimeException("BOOM!");
+        }
     }
 }

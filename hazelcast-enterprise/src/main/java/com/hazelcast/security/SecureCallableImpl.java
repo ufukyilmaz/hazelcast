99911a8677129684906ad2258d764d914d00cb7c
Viliam Durina <viliam@hazelcast.com>
2017-12-11 14:03:53 +0100

    Follow changes

--- hazelcast-enterprise/src/main/java/com/hazelcast/security/SecureCallableImpl.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/security/SecureCallableImpl.java
@@ -9,7 +9,7 @@ import com.hazelcast.core.Cluster;
 import com.hazelcast.core.DistributedObject;
 import com.hazelcast.core.DistributedObjectListener;
 import com.hazelcast.core.Endpoint;
-import com.hazelcast.core.ReliableIdGenerator;
+import com.hazelcast.reliableidgen.ReliableIdGenerator;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.core.IAtomicLong;
--- hazelcast-enterprise/src/test/java/com/hazelcast/concurrent/reliableidgen/ReliableIdGenCompatibilityTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/concurrent/reliableidgen/ReliableIdGenCompatibilityTest.java
@@ -1,6 +1,7 @@
 package com.hazelcast.concurrent.reliableidgen;
 
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
+import com.hazelcast.reliableidgen.impl.ReliableIdGenerator_MemberIntegrationTest;
 import com.hazelcast.test.annotation.CompatibilityTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;

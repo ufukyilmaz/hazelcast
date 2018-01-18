7af3a54e8e5f43674020da2bf61af956830ef3e1
Ahmet Mircik <ahmetmircik@gmail.com>
2018-01-18 11:32:44 +0300

    Removed unused imports

--- hazelcast-enterprise/src/test/java/com/hazelcast/quorum/cache/HDCacheQuorumReadTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/quorum/cache/HDCacheQuorumReadTest.java
@@ -1,7 +1,6 @@
 package com.hazelcast.quorum.cache;
 
 import com.hazelcast.enterprise.EnterpriseParametersRunnerFactory;
-import com.hazelcast.quorum.map.MapQuorumReadTest;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.AfterClass;
@@ -9,7 +8,6 @@ import org.junit.BeforeClass;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import sun.misc.Cache;
 
 import static com.hazelcast.HDTestSupport.getHDConfig;
 

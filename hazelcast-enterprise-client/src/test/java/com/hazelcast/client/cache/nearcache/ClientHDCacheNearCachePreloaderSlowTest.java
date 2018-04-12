9fb19df7a7268462b579fda97d8d5637298d3333
donnerbart <github@thunderphreak.de>
2018-04-13 00:48:47 +0200

    Removed OS header from test classes

--- hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/nearcache/ClientHDCacheNearCachePreloaderSlowTest.java
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/nearcache/ClientHDCacheNearCachePreloaderSlowTest.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hazelcast.client.cache.nearcache;
 
 import com.hazelcast.client.config.ClientConfig;
@@ -25,6 +9,8 @@ import com.hazelcast.test.annotation.SlowTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 import java.util.Collection;
 
@@ -33,11 +19,11 @@ import static com.hazelcast.internal.nearcache.HiDensityNearCacheTestUtils.getNe
 import static java.util.Arrays.asList;
 
 @RunWith(Parameterized.class)
-@Parameterized.UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
+@UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
 @Category({SlowTest.class})
 public class ClientHDCacheNearCachePreloaderSlowTest extends ClientCacheNearCachePreloaderSlowTest {
 
-    @Parameterized.Parameters(name = "format:{0} invalidationOnChange:{1} serializeKeys:{2}")
+    @Parameters(name = "format:{0} invalidationOnChange:{1} serializeKeys:{2}")
     public static Collection<Object[]> parameters() {
         return asList(new Object[][]{
                 {InMemoryFormat.NATIVE, false, true},
@@ -68,5 +54,4 @@ public class ClientHDCacheNearCachePreloaderSlowTest extends ClientCacheNearCach
                 .setLicenseKey(SampleLicense.UNLIMITED_LICENSE)
                 .setNativeMemoryConfig(createNativeMemoryConfig());
     }
-
 }

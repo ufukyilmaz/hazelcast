63fdfc137cf7da6177c963ee70ff3652799f3592
Matko Medenjak <medenjak.matko@gmail.com>
2018-04-13 14:01:51 +0200

    Fix EnterpriseMemberListJoinVersionTest (#2062)
    * Fix EnterpriseMemberListJoinVersionTest
    
    The cluster version change would fail because there were ongoing
    migrations because of membership changes. We wait for membership
    changes and then change the cluster version.
    Also, the test was
    throwing deserialization exceptions because a 3.10 member was pretending
    to be a 3.9 member and sending a 3.9 PostJoinMapOperation while other
    3.10 members deserialized it as a class being sent by a 3.10 member.
    This did not affect the test outcome but we clean it up nevertheless.
    
    Fixes: https://github.com/hazelcast/hazelcast-enterprise/issues/2060

--- hazelcast-enterprise/src/test/java/com/hazelcast/internal/cluster/impl/EnterpriseMemberListJoinVersionTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/internal/cluster/impl/EnterpriseMemberListJoinVersionTest.java
@@ -1,7 +1,10 @@
 package com.hazelcast.internal.cluster.impl;
 
+import com.hazelcast.cluster.oldmembersupport.MapDataSerializerHookWithPostJoinMapOperation39;
+import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.map.impl.MapDataSerializerHook;
 import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
@@ -13,8 +16,8 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import static com.hazelcast.instance.BuildInfoProvider.HAZELCAST_INTERNAL_OVERRIDE_VERSION;
-import static com.hazelcast.internal.cluster.Versions.V3_10;
-import static com.hazelcast.internal.cluster.Versions.V3_9;
+import static com.hazelcast.internal.cluster.Versions.CURRENT_CLUSTER_VERSION;
+import static com.hazelcast.internal.cluster.Versions.PREVIOUS_CLUSTER_VERSION;
 import static com.hazelcast.internal.cluster.impl.MemberListJoinVersionTest.assertJoinMemberListVersions;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
@@ -30,11 +33,16 @@ public class EnterpriseMemberListJoinVersionTest extends HazelcastTestSupport {
 
     @Test
     public void when_clusterIsUpgradedTo310_then_allNodesReceiveJoinVersionsEventually() {
-        System.setProperty(HAZELCAST_INTERNAL_OVERRIDE_VERSION, V3_9.toString());
+        System.setProperty(HAZELCAST_INTERNAL_OVERRIDE_VERSION, PREVIOUS_CLUSTER_VERSION.toString());
 
-        final HazelcastInstance member1 = factory.newHazelcastInstance();
+        // we must use the overloaded classloader because one 3.10 member is "faking"
+        // to be a 3.9 member by using version override. It must then send a 3.9 PostJoinMapOperation,
+        // since other members know it is not a 3.9 member.
+        // see: https://github.com/hazelcast/hazelcast-enterprise/issues/1997#issuecomment-377932234
+        // RU_COMPAT_3_9
+        final HazelcastInstance member1 = factory.newHazelcastInstance(configWithOverloadedClassLoader());
 
-        System.setProperty(HAZELCAST_INTERNAL_OVERRIDE_VERSION, V3_10.toString());
+        System.setProperty(HAZELCAST_INTERNAL_OVERRIDE_VERSION, CURRENT_CLUSTER_VERSION.toString());
 
         final HazelcastInstance member2 = factory.newHazelcastInstance();
         final HazelcastInstance member3 = factory.newHazelcastInstance();
@@ -44,13 +52,26 @@ public class EnterpriseMemberListJoinVersionTest extends HazelcastTestSupport {
         member1.getLifecycleService().terminate();
 
         assertClusterSizeEventually(2, member2, member3);
+        waitAllForSafeState(member2, member3);
 
-        member2.getCluster().changeClusterVersion(V3_10);
+        member2.getCluster().changeClusterVersion(CURRENT_CLUSTER_VERSION);
 
         assertTrueEventually(new AssertTask() {
             @Override
             public void run() throws Exception {
                 assertJoinMemberListVersions(member2, member3);
+            }
+        });
+    }
+
+    private Config configWithOverloadedClassLoader() {
+        return getConfig().setClassLoader(new ClassLoader() {
+            @Override
+            protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+                if (name.equals(MapDataSerializerHook.class.getName())) {
+                    return super.loadClass(MapDataSerializerHookWithPostJoinMapOperation39.class.getName());
+                }
+                return super.loadClass(name, resolve);
             }
         });
     }

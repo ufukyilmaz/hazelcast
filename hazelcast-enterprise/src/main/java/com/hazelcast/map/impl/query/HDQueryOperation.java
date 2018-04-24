27ed01520ffccb9697a7840331df0fde40767a18
Peter Veentjer <alarmnummer@gmail.com>
2018-04-24 15:10:41 +0300

    Converted HDQueryOperation to new batch functionality. (#2066)
    The old batch functionality was problematic since it runs
    all partitions for a partition thread without the ability
    to interleave other operations. This leads to very bad
    response times if e.g. a map.get is done while a hd query
    is being executed.
    
    With the new batch functionality, this problem is gone.

--- hazelcast-enterprise/src/main/java/com/hazelcast/map/impl/query/HDQueryOperation.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/map/impl/query/HDQueryOperation.java
@@ -2,38 +2,47 @@ package com.hazelcast.map.impl.query;
 
 import com.hazelcast.core.ExecutionCallback;
 import com.hazelcast.core.MemberLeftException;
-import com.hazelcast.map.impl.MapServiceContext;
-import com.hazelcast.map.impl.operation.EnterpriseMapDataSerializerHook;
 import com.hazelcast.map.impl.operation.MapOperation;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.spi.CallStatus;
 import com.hazelcast.spi.ExceptionAction;
 import com.hazelcast.spi.ReadonlyOperation;
 import com.hazelcast.spi.exception.TargetNotMemberException;
+import com.hazelcast.spi.impl.AbstractCompletableFuture;
+import com.hazelcast.spi.impl.operationservice.PartitionTaskFactory;
 import com.hazelcast.spi.impl.operationservice.impl.OperationServiceImpl;
-import com.hazelcast.spi.impl.operationservice.impl.batching.PartitionAwareCallable;
-import com.hazelcast.spi.impl.operationservice.impl.batching.PartitionAwareCallableBatchingRunnable;
-import com.hazelcast.spi.impl.operationservice.impl.batching.PartitionAwareCallableFactory;
+import com.hazelcast.spi.partition.IPartition;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
+import static com.hazelcast.map.impl.operation.EnterpriseMapDataSerializerHook.F_ID;
+import static com.hazelcast.map.impl.operation.EnterpriseMapDataSerializerHook.QUERY_OP;
+import static com.hazelcast.spi.CallStatus.DONE_RESPONSE;
+import static com.hazelcast.spi.CallStatus.OFFLOADED;
 import static com.hazelcast.spi.ExceptionAction.THROW_EXCEPTION;
 import static com.hazelcast.util.ExceptionUtil.rethrow;
+import static java.util.Arrays.copyOfRange;
 
 /**
  * Query operation that runs a query for the HD memory.
- * It is run on a generic thread, and then it spawns partition-query-operations for each local partition.
- * This operation does not return any result until all the partition-query-operations have finished.
- * Then it return a merged result.
- * The main reason for this design is that we want the partition-query-operations to be local. In this way we skip
- * the network overhead. So each member gets a single HDQueryOperation and then it queries all local partitions
- * on partitions threads.
+ *
+ * It is run on a generic thread, and then it spawns partition-query-operations
+ * for each local partition. This operation does not return any result until all
+ * the partition-query-operations have finished. Then it return a merged result.
+ *
+ * The main reason for this design is that we want the partition-query-operations
+ * to be local. In this way we skip the network overhead. So each member gets a
+ * single HDQueryOperation and then it queries all local partitions on partitions
+ * threads.
  */
 public class HDQueryOperation extends MapOperation implements ReadonlyOperation {
 
     private Query query;
+    private transient Result result;
 
     public HDQueryOperation() {
     }
@@ -44,99 +53,48 @@ public class HDQueryOperation extends MapOperation implements ReadonlyOperation 
     }
 
     @Override
-    public void run() {
+    public CallStatus call() {
         QueryRunner queryRunner = mapServiceContext.getMapQueryRunner(getName());
-        runAsyncPartitionThreadScanForNative(queryRunner);
-    }
 
-    private void runAsyncPartitionThreadScanForNative(QueryRunner queryRunner) {
-        final OperationServiceImpl ops = (OperationServiceImpl) getNodeEngine().getOperationService();
-        ops.onStartAsyncOperation(this);
-        runPartitionScanOnPartitionThreadsAsyncBatched(query, queryRunner);
-    }
-
-    void runPartitionScanOnPartitionThreadsAsyncBatched(final Query query, final QueryRunner queryRunner) {
-        final OperationServiceImpl ops = (OperationServiceImpl) getNodeEngine().getOperationService();
-        PartitionAwareCallableBatchingRunnable queryRunnable = new PartitionAwareCallableBatchingRunnable(
-                getNodeEngine(), new QueryPartitionCallableFactory(query, mapServiceContext));
-
-        ops.getOperationExecutor().executeOnPartitionThreads(queryRunnable);
-        queryRunnable.getFuture().andThen(
-                new ExecutionCallback<Object>() {
-                    @Override
-                    public void onResponse(Object response) {
-                        try {
-                            Result modifiableResult;
-                            try {
-                                modifiableResult = queryRunner.populateEmptyResult(query, Collections.<Integer>emptyList());
-                                populateResult((List) response, modifiableResult);
-                            } catch (Exception e) {
-                                HDQueryOperation.this.sendResponse(e);
-                                throw rethrow(e);
-                            }
-                            HDQueryOperation.this.sendResponse(modifiableResult);
-                        } finally {
-                            ops.onCompletionAsyncOperation(HDQueryOperation.this);
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Throwable t) {
-                        try {
-                            HDQueryOperation.this.sendResponse(t);
-                        } finally {
-                            ops.onCompletionAsyncOperation(HDQueryOperation.this);
-                        }
-                    }
-                });
-    }
-
-    private static class QueryPartitionCallableFactory implements PartitionAwareCallableFactory {
-        private final Query query;
-        private final MapServiceContext mapServiceContext;
-
-        public QueryPartitionCallableFactory(Query query, MapServiceContext mapServiceContext) {
-            this.query = query;
-            this.mapServiceContext = mapServiceContext;
+        int[] localPartitions = localPartitions();
+        if (localPartitions.length == 0) {
+            // important to deal with situation of not having any
+            this.result = queryRunner.populateEmptyResult(query, Collections.<Integer>emptyList());
+            return DONE_RESPONSE;
         }
 
-        @Override
-        public PartitionAwareCallable create() {
-            return new QueryPartitionCallable(query, mapServiceContext);
-        }
-    }
-
-    private static class QueryPartitionCallable implements PartitionAwareCallable {
-        private final MapServiceContext mapServiceContext;
-        private final Query query;
-
-        public QueryPartitionCallable(Query query, MapServiceContext mapServiceContext) {
-            this.query = query;
-            this.mapServiceContext = mapServiceContext;
-        }
-
-        @Override
-        public Object call(int partitionId) {
-            QueryRunner queryRunner = mapServiceContext.getMapQueryRunner(query.getMapName());
-            return queryRunner.runPartitionIndexOrPartitionScanQueryOnGivenOwnedPartition(query, partitionId);
-        }
-    }
-
-    private Result populateResult(List resultObjects, Result result) {
-        if (resultObjects == null) {
-            return result;
-        }
-        for (Object resultObject : resultObjects) {
-            if (resultObject instanceof Result) {
-                Result partitionResult = (Result) resultObject;
-                result.combine(partitionResult);
-            }
-        }
-        return result;
+        QueryFuture future = new QueryFuture(localPartitions.length);
+        OperationServiceImpl operationService = getOperationService();
+        operationService.onStartAsyncOperation(this);
+        operationService.execute(new QueryTaskFactory(query, future), localPartitions);
+        future.andThen(new ExecutionCallbackImpl(queryRunner, query));
+        return OFFLOADED;
     }
 
     @Override
-    public void afterRun() throws Exception {
+    public Object getResponse() {
+        return result;
+    }
+
+    private int partitionCount() {
+        return getNodeEngine().getPartitionService().getPartitionCount();
+    }
+
+    private OperationServiceImpl getOperationService() {
+        return (OperationServiceImpl) getNodeEngine().getOperationService();
+    }
+
+    private int[] localPartitions() {
+        int[] partitions = new int[partitionCount()];
+        int index = 0;
+        for (IPartition partition : getNodeEngine().getPartitionService().getPartitions()) {
+            if (partition.isLocal()) {
+                partitions[index] = partition.getPartitionId();
+                index++;
+            }
+        }
+
+        return copyOfRange(partitions, 0, index);
     }
 
     @Override
@@ -148,21 +106,22 @@ public class HDQueryOperation extends MapOperation implements ReadonlyOperation 
     }
 
     @Override
-    public Object getResponse() {
-        return null;
-    }
-
-    @Override
-    public boolean returnsResponse() {
-        return false;
-    }
-
-    @Override
     public void onExecutionFailure(Throwable e) {
-        // This is required since if the returnsResponse() method returns false there won't be any response sent
-        // to the invoking party - this means that the operation won't be retried if the exception is instanceof
+        // This is required since if the returnsResponse() method returns false
+        // there won't be any response sent to the invoking party - this means
+        // that the operation won't be retried if the exception is instanceof
         // HazelcastRetryableException
         sendResponse(e);
+    }
+
+    @Override
+    public int getId() {
+        return QUERY_OP;
+    }
+
+    @Override
+    public final int getFactoryId() {
+        return F_ID;
     }
 
     @Override
@@ -177,13 +136,113 @@ public class HDQueryOperation extends MapOperation implements ReadonlyOperation 
         query = in.readObject();
     }
 
-    @Override
-    public int getId() {
-        return EnterpriseMapDataSerializerHook.QUERY_OP;
+    private class QueryFuture extends AbstractCompletableFuture {
+        private final AtomicReferenceArray<Result> resultArray = new AtomicReferenceArray<Result>(partitionCount());
+        private final AtomicInteger remaining;
+
+        QueryFuture(int localPartitionCount) {
+            super(getNodeEngine(), getLogger());
+            this.remaining = new AtomicInteger(localPartitionCount);
+        }
+
+        void addResult(int partitionId, Result result) {
+            if (result != null) {
+                resultArray.set(partitionId, result);
+            }
+
+            if (remaining.decrementAndGet() == 0) {
+                setResult(resultArray);
+            }
+        }
+
+        void completeExceptionally(Throwable error) {
+            super.setResult(error);
+        }
     }
 
-    @Override
-    public final int getFactoryId() {
-        return EnterpriseMapDataSerializerHook.F_ID;
+    private class QueryTaskFactory implements PartitionTaskFactory {
+        private final Query query;
+        private final QueryFuture future;
+
+        QueryTaskFactory(Query query, QueryFuture future) {
+            this.query = query;
+            this.future = future;
+        }
+
+        @Override
+        public Object create(int partitionId) {
+            return new QueryTask(query, partitionId, future);
+        }
+    }
+
+    private class QueryTask implements Runnable {
+        private final Query query;
+        private final int partitionId;
+        private final QueryFuture future;
+
+        QueryTask(Query query, int partitionId, QueryFuture future) {
+            this.query = query;
+            this.partitionId = partitionId;
+            this.future = future;
+        }
+
+        @Override
+        public void run() {
+            IPartition partition = getNodeEngine().getPartitionService().getPartition(partitionId);
+            if (!partition.isLocal()) {
+                future.addResult(partitionId, null);
+                return;
+            }
+
+            try {
+                QueryRunner queryRunner = mapServiceContext.getMapQueryRunner(query.getMapName());
+                Result result = queryRunner.runPartitionIndexOrPartitionScanQueryOnGivenOwnedPartition(query, partitionId);
+                future.addResult(partitionId, result);
+            } catch (Exception ex) {
+                future.completeExceptionally(ex);
+            }
+        }
+    }
+
+    private class ExecutionCallbackImpl implements ExecutionCallback<AtomicReferenceArray<Result>> {
+        private final QueryRunner queryRunner;
+        private final Query query;
+
+        ExecutionCallbackImpl(QueryRunner queryRunner, Query query) {
+            this.queryRunner = queryRunner;
+            this.query = query;
+        }
+
+        @Override
+        public void onResponse(AtomicReferenceArray<Result> response) {
+            try {
+                Result combinedResult = queryRunner.populateEmptyResult(query, Collections.<Integer>emptyList());
+                populateResult(response, combinedResult);
+                HDQueryOperation.this.sendResponse(combinedResult);
+            } catch (Exception e) {
+                HDQueryOperation.this.sendResponse(e);
+                throw rethrow(e);
+            } finally {
+                getOperationService().onCompletionAsyncOperation(HDQueryOperation.this);
+            }
+        }
+
+        private void populateResult(AtomicReferenceArray<Result> resultArray, Result combinedResult) {
+            for (int k = 0; k < resultArray.length(); k++) {
+                Result partitionResult = resultArray.get(k);
+                if (partitionResult != null) {
+                    combinedResult.combine(partitionResult);
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            try {
+                HDQueryOperation.this.sendResponse(t);
+            } finally {
+                getOperationService().onCompletionAsyncOperation(HDQueryOperation.this);
+            }
+        }
     }
 }

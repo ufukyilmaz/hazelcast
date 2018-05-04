00b82c5cfd42771380e4e1b441a94c98f264aa5d
Vassilis Bekiaris <vbekiaris@gmail.com>
2018-05-04 13:08:24 +0300

    Enable previously ignored semaphore compatibility tests

--- hazelcast-enterprise/src/test/java/com/hazelcast/concurrent/semaphore/SemaphoreCompatibilityTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/concurrent/semaphore/SemaphoreCompatibilityTest.java
@@ -3,8 +3,6 @@ package com.hazelcast.concurrent.semaphore;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
 import com.hazelcast.test.annotation.CompatibilityTest;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
@@ -14,20 +12,6 @@ import org.junit.runner.RunWith;
 @RunWith(EnterpriseSerialJUnitClassRunner.class)
 @Category(CompatibilityTest.class)
 public class SemaphoreCompatibilityTest extends SemaphoreBasicTest {
-
-    // RU_COMPAT_3_9
-    @Ignore("Operation increase permits was added in 3.10")
-    @Test
-    @Override
-    public void testIncreasePermits() {
-    }
-
-    // RU_COMPAT_3_9
-    @Ignore("Operation increase permits was added in 3.10")
-    @Test
-    @Override
-    public void testIncrease_whenArgumentNegative() {
-    }
 
     @Override
     protected HazelcastInstance[] newInstances() {

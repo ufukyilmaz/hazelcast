a4e0cf4688867b435ceff18d9fc84cbdb934bf43
Zoltan Baranyi <blazember@gmail.com>
2018-05-15 14:51:07 +0200

    Fix flaky behavior of AbstractWanReplication#isConnected
    Change behavior of `AbstractWanReplication#isConnected` to report connected only if there is at least one alive connection over WAN. Before the fix it reported connected if there was at least one endpoint, which was true even if there was only a connection attempt in progress.
    
    Fixes #2074

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/wan/map/WanConnectedTest.java
@@ -0,0 +1,89 @@
+package com.hazelcast.wan.map;
+
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.WanPublisherConfig;
+import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
+import com.hazelcast.enterprise.wan.EnterpriseWanReplicationService;
+import com.hazelcast.enterprise.wan.WanReplicationEndpoint;
+import com.hazelcast.enterprise.wan.WanReplicationPublisherDelegate;
+import com.hazelcast.enterprise.wan.replication.WanBatchReplication;
+import com.hazelcast.map.merge.PassThroughMergePolicy;
+import com.hazelcast.test.AssertTask;
+import com.hazelcast.test.annotation.SlowTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import java.util.Map;
+
+import static com.hazelcast.enterprise.wan.replication.WanReplicationProperties.ENDPOINTS;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(EnterpriseSerialJUnitClassRunner.class)
+@Category(SlowTest.class)
+public class WanConnectedTest extends MapWanReplicationTestSupport {
+
+    @Test
+    public void testIsConnectionTrueWhenWellConfigured() {
+        String setupName = "atob";
+        setupReplicateFrom(configA, configB, clusterB.length, setupName, PassThroughMergePolicy.class.getName());
+
+        startClusterA();
+        startClusterB();
+
+        final WanReplicationEndpoint endpoint = getWanReplicationDelegate(setupName).getEndpoint("B");
+
+        createDataIn(clusterA, "map", 1, 10);
+
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertTrue(endpoint.getStats().isConnected());
+            }
+        });
+    }
+
+    @Test
+    public void testIsConnectionFalseWhenMisconfigured() {
+        String setupName = "atob";
+        setupReplicateFrom(configA, configB, clusterB.length, setupName, PassThroughMergePolicy.class.getName());
+
+        for (WanPublisherConfig publisherConfig : configA.getWanReplicationConfig(setupName).getWanPublisherConfigs()) {
+            final Map<String, Comparable> properties = publisherConfig.getProperties();
+            final String endpoints = (String) properties.get(ENDPOINTS.key());
+            final String endpointsWithError = endpoints.replaceAll(":58", ":59");
+            properties.put(ENDPOINTS.key(), endpointsWithError);
+        }
+
+        startClusterA();
+        startClusterB();
+
+        final WanReplicationEndpoint endpoint = getWanReplicationDelegate(setupName).getEndpoint("B");
+
+        createDataIn(clusterA, "map", 1, 10);
+
+        assertTrueAllTheTime(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertFalse(endpoint.getStats().isConnected());
+            }
+        }, 10);
+    }
+
+    private WanReplicationPublisherDelegate getWanReplicationDelegate(String publisherName) {
+        EnterpriseWanReplicationService wanReplicationService
+                = (EnterpriseWanReplicationService) getNode(clusterA[0]).nodeEngine.getWanReplicationService();
+        return (WanReplicationPublisherDelegate) wanReplicationService.getWanReplicationPublisher(publisherName);
+    }
+
+    @Override
+    public String getReplicationImpl() {
+        return WanBatchReplication.class.getName();
+    }
+
+    @Override
+    public InMemoryFormat getMemoryFormat() {
+        return InMemoryFormat.OBJECT;
+    }
+}

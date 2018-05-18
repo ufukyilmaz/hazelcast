3f4364df4445fa91ab5582222798bbfd7da169cf
donnerbart <github@thunderphreak.de>
2018-05-18 10:42:08 +0200

    Cleanup of Near Cache reconciliation tests

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/invalidation/HDMemberMapReconciliationTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/invalidation/HDMemberMapReconciliationTest.java
@@ -7,6 +7,8 @@ import com.hazelcast.test.annotation.QuickTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 import java.util.Collection;
 
@@ -17,29 +19,29 @@ import static com.hazelcast.config.InMemoryFormat.OBJECT;
 import static java.util.Arrays.asList;
 
 @RunWith(Parameterized.class)
-@Parameterized.UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
+@UseParametersRunnerFactory(EnterpriseParametersRunnerFactory.class)
 @Category({QuickTest.class, ParallelTest.class})
 public class HDMemberMapReconciliationTest extends MemberMapReconciliationTest {
 
-    @Parameterized.Parameters(name = "mapInMemoryFormat:{0} nearCacheInMemoryFormat:{1}")
+    @Parameters(name = "mapInMemoryFormat:{0} nearCacheInMemoryFormat:{1}")
     public static Collection<Object[]> parameters() {
         return asList(new Object[][]{
                 {BINARY, BINARY},
                 {BINARY, OBJECT},
                 {BINARY, NATIVE},
+
                 {OBJECT, BINARY},
                 {OBJECT, OBJECT},
                 {OBJECT, NATIVE},
+
                 {NATIVE, BINARY},
                 {NATIVE, OBJECT},
                 {NATIVE, NATIVE},
         });
     }
 
-
     @Override
     protected Config getConfig() {
-        return getHDConfig();
+        return getHDConfig(super.getConfig());
     }
-
 }

7f805145cc76e3dec476bb785e8415d8fac83393
donnerbart <github@thunderphreak.de>
2018-05-18 10:42:08 +0200

    Cleanup of Near Cache invalidation member add remove tests
    * added missing HDMapNearCacheInvalidationMemberAddRemoveTest

---
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/map/impl/nearcache/invalidation/ClientHDMapNearCacheInvalidationMemberAddRemoveTest.java
@@ -0,0 +1,44 @@
+package com.hazelcast.client.map.impl.nearcache.invalidation;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.enterprise.SampleLicense;
+import com.hazelcast.test.annotation.NightlyTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+import static com.hazelcast.config.EvictionConfig.MaxSizePolicy.ENTRY_COUNT;
+import static com.hazelcast.internal.nearcache.HiDensityNearCacheTestUtils.createNativeMemoryConfig;
+
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
+@Category(NightlyTest.class)
+public class ClientHDMapNearCacheInvalidationMemberAddRemoveTest extends ClientMapInvalidationMemberAddRemoveTest {
+
+    @Override
+    protected Config createConfig() {
+        return getHDConfig(super.getConfig());
+    }
+
+    @Override
+    protected ClientConfig createClientConfig() {
+        return super.createClientConfig()
+                .setLicenseKey(SampleLicense.UNLIMITED_LICENSE)
+                .setNativeMemoryConfig(createNativeMemoryConfig());
+    }
+
+    @Override
+    protected NearCacheConfig createNearCacheConfig(String mapName) {
+        EvictionConfig evictionConfig = new EvictionConfig()
+                .setMaximumSizePolicy(ENTRY_COUNT)
+                .setSize(90);
+
+        return super.createNearCacheConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE)
+                .setEvictionConfig(evictionConfig);
+    }
+}
---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/invalidation/HDMapNearCacheInvalidationMemberAddRemoveTest.java
@@ -0,0 +1,41 @@
+package com.hazelcast.map.impl.nearcache.invalidation;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.test.annotation.NightlyTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+import static com.hazelcast.config.EvictionConfig.MaxSizePolicy.ENTRY_COUNT;
+
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
+@Category(NightlyTest.class)
+public class HDMapNearCacheInvalidationMemberAddRemoveTest extends MemberMapInvalidationMemberAddRemoveTest {
+
+    @Override
+    protected Config createConfig() {
+        return getHDConfig(super.getConfig());
+    }
+
+    @Override
+    protected MapConfig createMapConfig(String mapName) {
+        return super.createMapConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE);
+    }
+
+    @Override
+    protected NearCacheConfig createNearCacheConfig(String mapName) {
+        EvictionConfig evictionConfig = new EvictionConfig()
+                .setMaximumSizePolicy(ENTRY_COUNT)
+                .setSize(90);
+
+        return super.createNearCacheConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE)
+                .setEvictionConfig(evictionConfig);
+    }
+}

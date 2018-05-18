4abf0403aac679d699504b83585b8aaa7cd08da8
donnerbart <github@thunderphreak.de>
2018-05-18 10:42:08 +0200

    Cleanup of Near Cache metadata generator tests

---
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/cache/nearcache/invalidation/ClientHDCacheMetaDataGeneratorTest.java
@@ -0,0 +1,61 @@
+package com.hazelcast.client.cache.nearcache.invalidation;
+
+import com.hazelcast.client.cache.impl.nearcache.invalidation.ClientCacheMetaDataGeneratorTest;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.CacheConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.NativeMemoryConfig;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
+import com.hazelcast.memory.MemorySize;
+import com.hazelcast.memory.MemoryUnit;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+import static com.hazelcast.config.EvictionConfig.MaxSizePolicy.USED_NATIVE_MEMORY_PERCENTAGE;
+import static com.hazelcast.enterprise.SampleLicense.ENTERPRISE_HD_LICENSE;
+
+@RunWith(EnterpriseSerialJUnitClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class ClientHDCacheMetaDataGeneratorTest extends ClientCacheMetaDataGeneratorTest {
+
+    private static final MemorySize CLIENT_NATIVE_MEMORY_SIZE = new MemorySize(128, MemoryUnit.MEGABYTES);
+
+    @Override
+    protected Config getConfig() {
+        return getHDConfig(super.getConfig());
+    }
+
+    @Override
+    protected ClientConfig getClientConfig() {
+        NativeMemoryConfig nativeMemoryConfig = new NativeMemoryConfig()
+                .setEnabled(true)
+                .setSize(CLIENT_NATIVE_MEMORY_SIZE);
+
+        return super.getClientConfig()
+                .setLicenseKey(ENTERPRISE_HD_LICENSE)
+                .setNativeMemoryConfig(nativeMemoryConfig);
+    }
+
+    @Override
+    protected CacheConfig<Integer, Integer> getCacheConfig(String cacheName) {
+        EvictionConfig evictionConfig = new EvictionConfig()
+                .setMaximumSizePolicy(USED_NATIVE_MEMORY_PERCENTAGE)
+                .setSize(90);
+
+        return super.getCacheConfig(cacheName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE)
+                .setEvictionConfig(evictionConfig);
+    }
+
+    @Override
+    protected NearCacheConfig getNearCacheConfig() {
+        return super.getNearCacheConfig()
+                .setInMemoryFormat(InMemoryFormat.NATIVE);
+    }
+}
--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/invalidation/HDMapMetaDataGeneratorTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/invalidation/HDMapMetaDataGeneratorTest.java
@@ -1,19 +1,35 @@
 package com.hazelcast.map.impl.nearcache.invalidation;
 
-import com.hazelcast.HDTestSupport;
 import com.hazelcast.config.Config;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NearCacheConfig;
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import static com.hazelcast.HDTestSupport.getHDConfig;
+
 @RunWith(EnterpriseSerialJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
-public class HDMapMetaDataGeneratorTest extends MapMetaDataGeneratorTest {
+public class HDMapMetaDataGeneratorTest extends MemberMapMetaDataGeneratorTest {
 
     @Override
     protected Config getConfig() {
-        return HDTestSupport.getHDConfig(super.getConfig());
+        return getHDConfig(super.getConfig());
+    }
+
+    @Override
+    protected MapConfig getMapConfig(String mapName) {
+        return super.getMapConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE);
+    }
+
+    @Override
+    protected NearCacheConfig getNearCacheConfig(String mapName) {
+        return super.getNearCacheConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE);
     }
 }

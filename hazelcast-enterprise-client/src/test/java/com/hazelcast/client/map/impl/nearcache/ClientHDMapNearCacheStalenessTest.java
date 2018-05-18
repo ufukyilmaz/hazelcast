98b2a79f361e4fc76ab283b9973a4ed8a638b4f8
donnerbart <github@thunderphreak.de>
2018-05-18 10:42:08 +0200

    Cleanup of Near Cache staleness tests

---
+++ hazelcast-enterprise-client/src/test/java/com/hazelcast/client/map/impl/nearcache/ClientHDMapNearCacheStalenessTest.java
@@ -0,0 +1,46 @@
+package com.hazelcast.client.map.impl.nearcache;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.NativeMemoryConfig;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+import static com.hazelcast.config.EvictionConfig.MaxSizePolicy.USED_NATIVE_MEMORY_PERCENTAGE;
+import static com.hazelcast.internal.nearcache.HiDensityNearCacheTestUtils.createNativeMemoryConfig;
+
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class ClientHDMapNearCacheStalenessTest extends ClientMapNearCacheStalenessTest {
+
+    @Override
+    protected Config getConfig() {
+        return getHDConfig(super.getConfig());
+    }
+
+    @Override
+    protected ClientConfig getClientConfig(String mapName) {
+        NativeMemoryConfig memoryConfig = createNativeMemoryConfig();
+
+        return super.getClientConfig(mapName)
+                .setNativeMemoryConfig(memoryConfig);
+    }
+
+    @Override
+    protected NearCacheConfig getNearCacheConfig(String mapName) {
+        EvictionConfig evictionConfig = new EvictionConfig()
+                .setMaximumSizePolicy(USED_NATIVE_MEMORY_PERCENTAGE)
+                .setSize(90);
+
+        return super.getNearCacheConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE)
+                .setEvictionConfig(evictionConfig);
+    }
+}
---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/nearcache/HDMapNearCacheStalenessTest.java
@@ -0,0 +1,35 @@
+package com.hazelcast.map.impl.nearcache;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class HDMapNearCacheStalenessTest extends MapNearCacheStalenessTest {
+
+    @Override
+    protected Config getConfig(String mapName) {
+        return getHDConfig(super.getConfig(mapName));
+    }
+
+    @Override
+    protected MapConfig getMapConfig(String mapName) {
+        return super.getMapConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE);
+    }
+
+    @Override
+    protected NearCacheConfig getNearCacheConfig(String mapName) {
+        return super.getNearCacheConfig(mapName)
+                .setInMemoryFormat(InMemoryFormat.NATIVE);
+    }
+}

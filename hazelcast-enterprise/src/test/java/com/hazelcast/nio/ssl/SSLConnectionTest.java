6b74a2f0f16029031a0efc4d74efaf69d45bc8bb
Josef Cacek <josef.cacek@gmail.com>
2018-08-28 09:36:33 +0200

    Revert "Decrease load to workaround PR issues"
    This reverts commit 07b25d75318277418d601117ec0f7c7b34319a57.

--- hazelcast-enterprise/src/test/java/com/hazelcast/nio/ssl/SSLConnectionTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/nio/ssl/SSLConnectionTest.java
@@ -15,6 +15,7 @@ import com.hazelcast.logging.Logger;
 import com.hazelcast.spi.properties.GroupProperty;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestAwareInstanceFactory;
+import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -35,7 +36,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
-@Category({QuickTest.class})
+@Category({QuickTest.class, ParallelTest.class})
 public class SSLConnectionTest {
 
     private final ILogger logger = Logger.getLogger(getClass());

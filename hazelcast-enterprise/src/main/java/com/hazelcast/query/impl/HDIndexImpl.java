fdf88256871fd6e888af8eadd406682d109908f8
Sergey Sitnikov <serge.sitnikov@gmail.com>
2018-09-14 16:13:16 +0700

    Forward map value extractors to HD indexes (#2227)
    Forward map value extractors to HD indexes
    
    This change fixes two issues:
    
    1. The consistency issue: queryable entries returned by HD indexes were
    unaware of the map value extractors, so it was impossible to extract
    values provided by the extractors from the returned entries.
    
    2. The performance issue: Extractors.empty is a pretty expensive call
    and it was performed for every returned entry. According to a slightly
    modified version of HDIndexPerfTest the throughput is about 2 times
    higher now.
    
    Fixes: https://github.com/hazelcast/hazelcast-enterprise/issues/2109

--- hazelcast-enterprise/src/main/java/com/hazelcast/query/impl/HDIndexImpl.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/query/impl/HDIndexImpl.java
@@ -1,9 +1,14 @@
 package com.hazelcast.query.impl;
 
+import com.hazelcast.elastic.tree.MapEntryFactory;
 import com.hazelcast.internal.memory.MemoryAllocator;
+import com.hazelcast.internal.serialization.impl.NativeMemoryData;
 import com.hazelcast.monitor.impl.PerIndexStats;
+import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.nio.serialization.EnterpriseSerializationService;
 import com.hazelcast.query.impl.getters.Extractors;
+
+import static com.hazelcast.nio.serialization.DataType.HEAP;
 
 public class HDIndexImpl extends IndexImpl {
 
@@ -17,12 +22,44 @@ public class HDIndexImpl extends IndexImpl {
     public IndexStore createIndexStore(boolean ordered, PerIndexStats stats) {
         EnterpriseSerializationService ess = (EnterpriseSerializationService) ss;
         MemoryAllocator malloc = stats.wrapMemoryAllocator(ess.getCurrentMemoryAllocator());
-        return ordered ? new HDSortedIndexStore(ess, malloc) : new HDUnsortedIndexStore(ess, malloc);
+        MapEntryFactory<QueryableEntry> entryFactory = new OnHeapEntryFactory(ess, extractors);
+        return ordered ? new HDSortedIndexStore(ess, malloc, entryFactory) : new HDUnsortedIndexStore(ess, malloc, entryFactory);
     }
 
     @Override
     public void destroy() {
         indexStore.destroy();
         super.destroy();
+    }
+
+    /**
+     * Converts off-heap key-value pairs back to on-heap queryable entries.
+     */
+    private static class OnHeapEntryFactory implements MapEntryFactory<QueryableEntry> {
+        private final EnterpriseSerializationService ess;
+        private final Extractors extractors;
+
+        OnHeapEntryFactory(EnterpriseSerializationService ess, Extractors extractors) {
+            this.ess = ess;
+            this.extractors = extractors;
+        }
+
+        @Override
+        public CachedQueryEntry create(Data key, Data value) {
+            Data heapData = toHeapData(key);
+            Data heapValue = toHeapData(value);
+            return new CachedQueryEntry(ess, heapData, heapValue, extractors);
+        }
+
+        private Data toHeapData(Data data) {
+            if (data instanceof NativeMemoryData) {
+                NativeMemoryData nativeMemoryData = (NativeMemoryData) data;
+                if (nativeMemoryData.totalSize() == 0) {
+                    return null;
+                }
+                return ess.toData(nativeMemoryData, HEAP);
+            }
+            return data;
+        }
     }
 }

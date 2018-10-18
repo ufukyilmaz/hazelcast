714faeac5a7d282734c2c2aed99c221484e094dc
Josef Cacek <josef.cacek@gmail.com>
2018-10-18 14:25:02 +0200

    Remove testsuite workaround for incompatible Log4J2 version. It's handled on netty (ver 4.1.30) side now.

--- hazelcast-enterprise/src/test/java/com/hazelcast/TestEnvironmentUtil.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/TestEnvironmentUtil.java
@@ -2,11 +2,7 @@ package com.hazelcast;
 
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
-import com.hazelcast.nio.ssl.PatchedLog4j2NettyLoggerFactory;
 import io.netty.handler.ssl.OpenSsl;
-import io.netty.util.internal.logging.InternalLoggerFactory;
-
-import org.apache.logging.log4j.spi.ExtendedLoggerWrapper;
 
 import static com.hazelcast.nio.IOUtil.closeResource;
 import static com.hazelcast.nio.IOUtil.copy;
@@ -64,7 +60,6 @@ public class TestEnvironmentUtil {
      * Returns true if it's possible to run Hazelcast with OpenSSL on this platform and JVM.
      */
     public static boolean isOpenSslSupported() {
-        patchOpenSslLogging();
         return Boolean.getBoolean("openssl.enforce") || OpenSsl.isAvailable();
     }
 
@@ -99,34 +94,5 @@ public class TestEnvironmentUtil {
             }
         }
         return targetFile;
-    }
-
-    private static void patchOpenSslLogging() {
-        InternalLoggerFactory originFactory = InternalLoggerFactory.getDefaultFactory();
-        if (!originFactory.getClass().getName().equals("io.netty.util.internal.logging.Log4J2LoggerFactory")) {
-            // it's already patched or netty does not use log4j2 at all
-            return;
-        }
-
-        // Netty created Log4J2LoggerFactory this implies we have log4j2 on a classpath
-        // let's check what version is this
-        try {
-            ExtendedLoggerWrapper.class.getMethod("debug", String.class, Object.class);
-            // apparently we are using a new log4j2 version! This means we live in the future
-            // and no longer supporting Java 6. Hence this whole netty logging patching should
-            // be removed!
-            throw new AssertionError("You managed to upgrade log4j2, congratulations!"
-                    + " Now let's removing the ugly netty logging patching!");
-        } catch (NoSuchMethodException e) {
-            // ok, this is an old version of log4j2. let's do some ugly patching
-
-            // reasoning:
-            // log4j2 2.3 is the last version supporting Java 6. However it's incompatible
-            // with Netty - some methods are missing and this results in AbstractMethodError
-            // so let's patch the Netty logging to use our wrapper which implements the missing
-            // methods on its own.
-            InternalLoggerFactory wrappedFactory = new PatchedLog4j2NettyLoggerFactory(originFactory);
-            InternalLoggerFactory.setDefaultFactory(wrappedFactory);
-        }
     }
 }

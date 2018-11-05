fd518b7e59d7a06be08f609251c6ff09aa4c2c02
sancar <sancar@hazelcast.com>
2018-11-05 17:18:54 +0300

    Tests for ssl trusting system default trust stores
    related to https://github.com/hazelcast/hazelcast/pull/14062

--- hazelcast-enterprise/src/test/java/com/hazelcast/nio/ssl/OpenSSLConnectionKeyManagerFactoryTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/nio/ssl/OpenSSLConnectionKeyManagerFactoryTest.java
@@ -1,22 +1,20 @@
 package com.hazelcast.nio.ssl;
 
-import static com.hazelcast.TestEnvironmentUtil.isIbmJvm;
-import static org.junit.Assume.assumeFalse;
-
-import java.util.Properties;
-
-import org.junit.BeforeClass;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-
 import com.hazelcast.config.Config;
 import com.hazelcast.config.SSLConfig;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
 import com.hazelcast.spi.properties.GroupProperty;
 import com.hazelcast.test.annotation.QuickTest;
+import org.junit.BeforeClass;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.TestEnvironmentUtil.copyTestResource;
+import static com.hazelcast.TestEnvironmentUtil.isIbmJvm;
+import static org.junit.Assume.assumeFalse;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
-@Category({ QuickTest.class })
+@Category({QuickTest.class})
 public class OpenSSLConnectionKeyManagerFactoryTest extends OpenSSLConnectionTest {
 
     @BeforeClass
@@ -25,12 +23,14 @@ public class OpenSSLConnectionKeyManagerFactoryTest extends OpenSSLConnectionTes
     }
 
     protected Config newConfig() {
-        Properties sslProperties = TestKeyStoreUtil.createSslProperties();
-
-        SSLConfig sslConfig = new SSLConfig()
-                .setEnabled(true)
-                .setFactoryImplementation(new OpenSSLEngineFactory())
-                .setProperties(sslProperties);
+        SSLConfig sslConfig = new SSLConfig().setEnabled(true);
+        sslConfig.setFactoryImplementation(new OpenSSLEngineFactory())
+                .setProperty("keyFile",
+                        copyTestResource(SSLConnectionTest.class, tempFolder.getRoot(), "privkey.pem").getAbsolutePath())
+                .setProperty("keyCertChainFile",
+                        copyTestResource(SSLConnectionTest.class, tempFolder.getRoot(), "fullchain.pem").getAbsolutePath())
+                .setProperty("trustCertCollectionFile",
+                        copyTestResource(SSLConnectionTest.class, tempFolder.getRoot(), "chain.pem").getAbsolutePath());
 
         Config config = new Config().setProperty(GroupProperty.IO_THREAD_COUNT.getName(), "1");
         config.getNetworkConfig().setSSLConfig(sslConfig);

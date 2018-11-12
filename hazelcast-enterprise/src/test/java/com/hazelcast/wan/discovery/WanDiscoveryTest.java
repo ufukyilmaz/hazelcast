5b771c842022b5dde790687269d76d9a50efa0e8
Matko Medenjak <medenjak.matko@gmail.com>
2018-11-12 09:28:58 +0100

    Enable fixed WanDiscoveryTest (#2579)
    Test was fixed with https://github.com/hazelcast/hazelcast/pull/14085

--- hazelcast-enterprise/src/test/java/com/hazelcast/wan/discovery/WanDiscoveryTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/wan/discovery/WanDiscoveryTest.java
@@ -26,7 +26,6 @@ import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import com.hazelcast.wan.map.MapWanReplicationTestSupport;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -44,7 +43,6 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
-@Ignore
 public class WanDiscoveryTest extends MapWanReplicationTestSupport {
 
     private String wanReplicationName;

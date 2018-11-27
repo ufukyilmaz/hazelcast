8547e5797bbbe51b0b69490717acfa7097206dbb
Vassilis Bekiaris <vbekiaris@gmail.com>
2018-11-27 18:06:21 +0200

    Removes explicit usage of 3.10.5 in MultiMapSplitBrainCompatTest
    Explicit usage of 3.10.5 was introduced in 3.11 dev cycle because an
    issue with multimap merge was fixed since 3.10.5.
    The test can now run with default compatibility test cluster including
    3.11 & 3.12-SNAPSHOT members.

--- hazelcast-enterprise/src/test/java/com/hazelcast/multimap/MultiMapSplitBrainCompatibilityTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/multimap/MultiMapSplitBrainCompatibilityTest.java
@@ -1,37 +1,16 @@
 package com.hazelcast.multimap;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.enterprise.EnterpriseParallelParametersRunnerFactory;
-import com.hazelcast.test.CompatibilityTestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.CompatibilityTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
-import static com.hazelcast.test.CompatibilityTestHazelcastInstanceFactory.CURRENT_VERSION;
-
 @RunWith(Parameterized.class)
 @UseParametersRunnerFactory(EnterpriseParallelParametersRunnerFactory.class)
 @Category(CompatibilityTest.class)
 public class MultiMapSplitBrainCompatibilityTest extends MultiMapSplitBrainTest {
-
-    public static final String V3_10_5 = "3.10.5";
-
-    // Test with 3.10.5 + 3.11 members because
-    // https://github.com/hazelcast/hazelcast/issues/13559 obstructs MultiMap
-    // split-brain healing and is fixed since 3.10.5
-    @Override
-    protected HazelcastInstance[] startInitialCluster(Config config, int clusterSize) {
-        HazelcastInstance[] hazelcastInstances = new HazelcastInstance[clusterSize];
-        factory = new CompatibilityTestHazelcastInstanceFactory(new String[] {V3_10_5, V3_10_5, CURRENT_VERSION});
-        for (int i = 0; i < clusterSize; i++) {
-            HazelcastInstance hz = factory.newHazelcastInstance(config);
-            hazelcastInstances[i] = hz;
-        }
-        return hazelcastInstances;
-    }
 
     @Override
     protected void onTearDown() {

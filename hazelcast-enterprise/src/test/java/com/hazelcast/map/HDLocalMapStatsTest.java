15bdff22b04cf9e885a832a4b3d7caf7d2450600
sancar <sancar@hazelcast.com>
2019-02-01 17:42:29 +0300

    Fix for hdlocalMapStats
    Owned entry count test is seperated.
    Because it creates the cluster with different config, it can not
    work with base class anymore.
    
    Query operations(keyset, values, entryset) seem to be updating]
    the stats per partition because of offloading, it seems more
    appropriate the fix the test in this case. Tests are ignored
    for now.
    
    related to #2704

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/HDLocalMapStatsTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDLocalMapStatsTest.java
@@ -2,12 +2,8 @@ package com.hazelcast.map;
 
 import com.hazelcast.cluster.ClusterState;
 import com.hazelcast.config.Config;
-import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
-import com.hazelcast.internal.serialization.impl.EnterpriseSerializationServiceV1;
-import com.hazelcast.memory.MemoryStats;
-import com.hazelcast.monitor.LocalMapStats;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Ignore;
@@ -19,12 +15,9 @@ import static com.hazelcast.HDTestSupport.getHDConfig;
 import static com.hazelcast.cluster.ClusterState.ACTIVE;
 import static com.hazelcast.cluster.ClusterState.FROZEN;
 import static com.hazelcast.cluster.ClusterState.PASSIVE;
-import static com.hazelcast.config.NativeMemoryConfig.MemoryAllocatorType.POOLED;
-import static org.junit.Assert.assertEquals;
 
 @RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
-@Ignore("https://github.com/hazelcast/hazelcast-enterprise/issues/2704")
 public class HDLocalMapStatsTest extends LocalMapStatsTest {
 
     // NOTE: all sizes below are as expected from the POOLED memory allocator
@@ -67,51 +60,24 @@ public class HDLocalMapStatsTest extends LocalMapStatsTest {
     }
 
     private void hdStatsCollectionAllowedWhenClusterState(ClusterState state) {
-        HazelcastInstance member = createHazelcastInstance(getConfig());
-        IMap map = member.getMap("test");
-
-        member.getCluster().changeClusterState(state);
+        IMap map = getMap();
+        instance.getCluster().changeClusterState(state);
         map.getLocalMapStats();
     }
 
     @Test
-    public void hdOwnedEntryCost() {
-        HazelcastInstance instance = createHazelcastInstance(getHDConfig(POOLED));
-        MemoryStats stats = ((EnterpriseSerializationServiceV1) getNodeEngineImpl(instance).getSerializationService())
-                .getMemoryManager().getMemoryStats();
-
-        IMap<String, Integer> map = instance.getMap("ownedEntryCostMap");
-
-        // single entry
-
-        map.put("key-0", 0);
-
-        LocalMapStats localMapStats = map.getLocalMapStats();
-        assertEquals(1, localMapStats.getOwnedEntryCount());
-        assertEquals(SINGLE_MAP_ENTRY_COST, localMapStats.getOwnedEntryMemoryCost());
-        assertEquals(BEHM_SLOT_COST + stats.getUsedNative(), localMapStats.getOwnedEntryMemoryCost());
-        assertEquals(0, localMapStats.getHeapCost());
-
-        // multiple entries
-
-        int numOfEntries = 5;
-        for (int i = 1; i < numOfEntries; i++) {
-            map.put("key-" + i, i);
-        }
-
-        localMapStats = map.getLocalMapStats();
-        assertEquals(numOfEntries, localMapStats.getOwnedEntryCount());
-        assertEquals(numOfEntries * SINGLE_MAP_ENTRY_COST, localMapStats.getOwnedEntryMemoryCost());
-        assertEquals(numOfEntries * BEHM_SLOT_COST + stats.getUsedNative(), localMapStats.getOwnedEntryMemoryCost());
-        assertEquals(0, localMapStats.getHeapCost());
-
-        // empty map
-
-        map.clear();
-
-        localMapStats = map.getLocalMapStats();
-        assertEquals(0, localMapStats.getOwnedEntryCount());
-        assertEquals(0, localMapStats.getOwnedEntryMemoryCost());
-        assertEquals(0, localMapStats.getHeapCost());
+    @Ignore("see https://github.com/hazelcast/hazelcast-enterprise/issues/2704")
+    public void testOtherOperationCount_entrySet() {
     }
+
+    @Test
+    @Ignore("see https://github.com/hazelcast/hazelcast-enterprise/issues/2704")
+    public void testOtherOperationCount_keySet() {
+    }
+
+    @Test
+    @Ignore("see https://github.com/hazelcast/hazelcast-enterprise/issues/2704")
+    public void testOtherOperationCount_values() {
+    }
+
 }

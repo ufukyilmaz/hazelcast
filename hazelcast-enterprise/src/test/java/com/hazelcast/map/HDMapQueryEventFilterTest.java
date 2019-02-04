32e4d95d7f8cd391f45e67de5cfefb089c3f9d3c
Matko Medenjak <matko@hazelcast.com>
2019-02-04 10:55:54 +0100

    Extract old value in SetOperation when natural event types enabled
    Fixes: https://github.com/hazelcast/hazelcast/issues/14445

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDMapQueryEventFilterTest.java
@@ -0,0 +1,20 @@
+package com.hazelcast.map;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class HDMapQueryEventFilterTest extends MapQueryEventFilterTest {
+
+    @Override
+    protected Config getConfig() {
+        return getHDConfig();
+    }
+}

60c3c573d241d602c4a74f6a645a2822d15616ec
Vassilis Bekiaris <vbekiaris@users.noreply.github.com>
2019-02-18 23:13:19 +0200

    Whitelists Jackson classes in data serializable conventions test (#2744)
    Shaded jackson classes must be omitted from serializable conventions
    tests. Because they are shaded, they can be only resolved by name
    at runtime.
    
    Fixes #2743

--- hazelcast-enterprise/src/test/java/com/hazelcast/internal/serialization/impl/EnterpriseDataSerializableConventionsTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/internal/serialization/impl/EnterpriseDataSerializableConventionsTest.java
@@ -3,7 +3,6 @@ package com.hazelcast.internal.serialization.impl;
 import com.hazelcast.cache.wan.WanCacheEntryView;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
@@ -14,13 +13,41 @@ import java.util.Set;
  */
 @RunWith(HazelcastParallelClassRunner.class)
 @Category({QuickTest.class})
-@Ignore("https://github.com/hazelcast/hazelcast-enterprise/issues/2743")
 public class EnterpriseDataSerializableConventionsTest extends DataSerializableConventionsTest {
+
+    private static final String[] WHITELISTED_CLASS_NAMES = new String[] {
+            "com.hazelcast.com.fasterxml.jackson.core.Base64Variant",
+            "com.hazelcast.com.fasterxml.jackson.core.JsonFactory",
+            "com.hazelcast.com.fasterxml.jackson.core.JsonLocation",
+            "com.hazelcast.com.fasterxml.jackson.core.JsonpCharacterEscapes",
+            "com.hazelcast.com.fasterxml.jackson.core.Version",
+            "com.hazelcast.com.fasterxml.jackson.core.io.SerializedString",
+            "com.hazelcast.com.fasterxml.jackson.core.util.DefaultIndenter",
+            "com.hazelcast.com.fasterxml.jackson.core.util.DefaultPrettyPrinter",
+            "com.hazelcast.com.fasterxml.jackson.core.util.DefaultPrettyPrinter$FixedSpaceIndenter",
+            "com.hazelcast.com.fasterxml.jackson.core.util.DefaultPrettyPrinter$NopIndenter",
+            "com.hazelcast.com.fasterxml.jackson.core.util.InternCache",
+            "com.hazelcast.com.fasterxml.jackson.core.util.MinimalPrettyPrinter",
+            "com.hazelcast.com.fasterxml.jackson.core.util.RequestPayload",
+            "com.hazelcast.com.fasterxml.jackson.core.util.Separators",
+    };
 
     @Override
     protected Set<Class> getWhitelistedClasses() {
         Set<Class> classes = super.getWhitelistedClasses();
         classes.add(WanCacheEntryView.class);
+        for (String className : WHITELISTED_CLASS_NAMES) {
+            addToWhiteList(classes, className);
+        }
         return classes;
+    }
+
+    // whitelist class accessible only by class name
+    private void addToWhiteList(Set<Class> whitelist, String className) {
+        try {
+            whitelist.add(Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

1c15d969a95d483b260484e4de51cde48fc7e148
Ahmet Mircik <ahmetmircik@gmail.com>
2019-03-04 18:12:05 +0300

    Extract record from HD lazy entry view

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/operation/HDEvictBatchBackupOperationTest.java
@@ -0,0 +1,20 @@
+package com.hazelcast.map.impl.operation;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.HDTestSupport.getHDConfig;
+
+@RunWith(EnterpriseSerialJUnitClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class HDEvictBatchBackupOperationTest extends EvictBatchBackupOperationTest {
+
+    @Override
+    protected Config getConfig() {
+        return getHDConfig(super.getConfig());
+    }
+}

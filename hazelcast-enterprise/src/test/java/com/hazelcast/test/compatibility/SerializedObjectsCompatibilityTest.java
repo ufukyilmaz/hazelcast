c6fecf6ac189c6ae1fb3f1bf68339489525a32c9
Vassilis Bekiaris <vbekiaris@gmail.com>
2019-03-07 16:13:20 +0200

    Ignores SerializedObjectsCompatibilityTest
    Reason: https://github.com/hazelcast/hazelcast/issues/14672

--- hazelcast-enterprise/src/test/java/com/hazelcast/test/compatibility/SerializedObjectsCompatibilityTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/test/compatibility/SerializedObjectsCompatibilityTest.java
@@ -20,6 +20,7 @@ import com.hazelcast.test.annotation.QuickTest;
 import com.hazelcast.test.compatibility.SerializedObjectsAccessor.SerializedObject;
 import com.hazelcast.version.Version;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -52,6 +53,7 @@ import static org.junit.Assume.assumeTrue;
 @RunWith(Parameterized.class)
 @UseParametersRunnerFactory(HazelcastParametersRunnerFactory.class)
 @Category({QuickTest.class, ParallelTest.class})
+@Ignore("https://github.com/hazelcast/hazelcast/issues/14672")
 public class SerializedObjectsCompatibilityTest extends HazelcastTestSupport {
 
     private static final String CLASSPATH_RESOURCE_PATTERN = "com/hazelcast/test/compatibility/serialized-objects-%s";

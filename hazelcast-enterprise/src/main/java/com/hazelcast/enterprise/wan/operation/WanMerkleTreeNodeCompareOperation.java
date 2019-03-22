60b8bc26263f6f2052c958a8084c438e61b6df2a
Zoltán Baranyi <blazember@gmail.com>
2019-03-22 15:23:25 +0100

    Use WAN executor for Merkle offloaded comparison (#2859)
    If we offload WanMerkleTreeNodeCompareOperation to the ASYNC executor,
    the offloaded operations may consume the whole threadpool, blocking
    other tasks, like invocation callbacks that the offloaded operation is
    waiting for, in which case the comparison starts hanging. This situation
    emerges if the source WAN cluster size equals to the ASYNC pool size,
    because every WAN source member sends an MT compare operation to the
    target cluster with its node hashes.
    
    This problem is fixed by using async invocations for MT comparison and executing
    the invocation callbacks in the WAN pool instead of the ASYNC pool, so MT
    comparison operations are no longer blocking callbacks.

--- hazelcast-enterprise/src/main/java/com/hazelcast/enterprise/wan/operation/WanMerkleTreeNodeCompareOperation.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/enterprise/wan/operation/WanMerkleTreeNodeCompareOperation.java
@@ -1,6 +1,8 @@
 package com.hazelcast.enterprise.wan.operation;
 
+import com.hazelcast.core.ExecutionCallback;
 import com.hazelcast.enterprise.wan.EWRDataSerializerHook;
+import com.hazelcast.enterprise.wan.replication.WanBatchReplication;
 import com.hazelcast.internal.cluster.impl.operations.WanReplicationOperation;
 import com.hazelcast.map.impl.MapService;
 import com.hazelcast.map.impl.operation.MerkleTreeNodeCompareOperationFactory;
@@ -8,24 +10,25 @@ import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.spi.CallStatus;
-import com.hazelcast.spi.ExecutionService;
 import com.hazelcast.spi.Offload;
 import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.OperationService;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.Executor;
+
+import static com.hazelcast.enterprise.wan.replication.WanBatchReplication.WAN_EXECUTOR;
 
 /**
  * Operation sent from the source WAN endpoint to the target endpoint.
  * This operation triggers comparison of merkle tree nodes. The result of
  * the comparison is an instance of {@link MerkleTreeNodeValueComparison}.
  * <p>
- * The comparison is offloaded to the {@link #EXECUTOR_NAME} executor.
+ * The comparison is offloaded to the {@link WanBatchReplication#WAN_EXECUTOR} wanExecutor.
  */
 public class WanMerkleTreeNodeCompareOperation extends Operation
         implements WanReplicationOperation, IdentifiedDataSerializable {
-    private static final String EXECUTOR_NAME = ExecutionService.ASYNC_EXECUTOR;
     private String mapName;
     private MerkleTreeNodeValueComparison remoteLevels;
 
@@ -47,35 +50,37 @@ public class WanMerkleTreeNodeCompareOperation extends Operation
      * A task for fetching local map merkle tree node values and comparing
      * them with the remote values.
      * The invocations for fetching local merkle tree values is offloaded
-     * to the {@link #EXECUTOR_NAME} executor.
+     * to the {@link WanBatchReplication#WAN_EXECUTOR} wanExecutor.
      */
-    private final class OffloadedMerkleTreeComparison extends Offload implements Runnable {
+    private final class OffloadedMerkleTreeComparison extends Offload implements ExecutionCallback<Map<Integer, int[]>> {
+
         private OffloadedMerkleTreeComparison() {
             super(WanMerkleTreeNodeCompareOperation.this);
         }
 
         @Override
         public void start() {
-            nodeEngine.getExecutionService()
-                      .getExecutor(EXECUTOR_NAME)
-                      .execute(this);
-        }
-
-        @Override
-        public void run() {
             try {
                 MerkleTreeNodeCompareOperationFactory factory = new MerkleTreeNodeCompareOperationFactory(mapName, remoteLevels);
                 OperationService os = getNodeEngine().getOperationService();
-                Map<Integer, int[]> diffNodes = os.invokeOnPartitions(
-                        MapService.SERVICE_NAME, factory, remoteLevels.getPartitionIds());
-                MerkleTreeNodeValueComparison comparison = new MerkleTreeNodeValueComparison(diffNodes);
-
-                WanMerkleTreeNodeCompareOperation.this.sendResponse(comparison);
+                Executor wanExecutor = nodeEngine.getExecutionService().getExecutor(WAN_EXECUTOR);
+                os.<int[]>invokeOnPartitionsAsync(MapService.SERVICE_NAME, factory, remoteLevels.getPartitionIds())
+                        .andThen(this, wanExecutor);
             } catch (Exception e) {
                 WanMerkleTreeNodeCompareOperation.this.sendResponse(e);
             }
         }
 
+        @Override
+        public void onResponse(Map<Integer, int[]> response) {
+            MerkleTreeNodeValueComparison comparison = new MerkleTreeNodeValueComparison(response);
+            WanMerkleTreeNodeCompareOperation.this.sendResponse(comparison);
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            WanMerkleTreeNodeCompareOperation.this.sendResponse(t);
+        }
     }
 
     @Override

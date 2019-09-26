c0d1caaa34e23de340a7631a360f8a5583cc6673
Ozan Tezcan <ozan.vx@gmail.com>
2019-09-26 12:29:12 +0300

    Removed pmem tests from parallel tests (#3166)

--- hazelcast-enterprise/src/test/java/com/hazelcast/client/map/impl/nearcache/ClientHDMapNearCacheBasicTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/client/map/impl/nearcache/ClientHDMapNearCacheBasicTest.java
@@ -2,76 +2,41 @@ package com.hazelcast.client.map.impl.nearcache;
 
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.config.Config;
-import com.hazelcast.config.NativeMemoryConfig;
-import com.hazelcast.enterprise.EnterpriseParallelParametersRunnerFactory;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
 import com.hazelcast.map.IMap;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Collection;
 
 import static com.hazelcast.HDTestSupport.getHDConfig;
 import static com.hazelcast.config.InMemoryFormat.NATIVE;
 import static com.hazelcast.config.NearCacheConfig.DEFAULT_SERIALIZE_KEYS;
-import static com.hazelcast.internal.memory.impl.PersistentMemoryHeap.PERSISTENT_MEMORY_CHECK_DISABLED_PROPERTY;
 import static com.hazelcast.internal.nearcache.HiDensityNearCacheTestUtils.createNativeMemoryConfig;
 import static com.hazelcast.internal.nearcache.NearCacheTestUtils.createNearCacheConfig;
-import static java.util.Arrays.asList;
 
 /**
  * Basic HiDensity Near Cache tests for {@link IMap} on Hazelcast clients.
  */
-@RunWith(Parameterized.class)
-@Parameterized.UseParametersRunnerFactory(EnterpriseParallelParametersRunnerFactory.class)
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class ClientHDMapNearCacheBasicTest extends ClientMapNearCacheBasicTest {
 
-    @Parameterized.Parameter
-    public String persistentMemoryDirectory;
-
-    @Parameterized.Parameters(name = "persistentMemoryDirectory: {0}")
-    public static Collection<Object[]> parameters() {
-        return asList(new Object[][]{
-                {PERSISTENT_MEMORY_DIRECTORY},
-                {null},
-        });
-    }
-
     @Override
     protected Config getConfig() {
-        return getHDConfig(persistentMemoryDirectory);
+        return getHDConfig(super.getConfig());
     }
 
     @Before
     @Override
     public void setUp() {
-        if (persistentMemoryDirectory != null) {
-            assumeThatLinuxOS();
-        }
         nearCacheConfig = createNearCacheConfig(NATIVE, DEFAULT_SERIALIZE_KEYS);
-    }
-
-    @BeforeClass
-    public static void init() {
-        System.setProperty(PERSISTENT_MEMORY_CHECK_DISABLED_PROPERTY, "true");
-    }
-
-    @AfterClass
-    public static void cleanup() {
-        System.clearProperty(PERSISTENT_MEMORY_CHECK_DISABLED_PROPERTY);
     }
 
     @Override
     protected ClientConfig getClientConfig() {
-        NativeMemoryConfig memoryConfig = createNativeMemoryConfig();
-        memoryConfig.setPersistentMemoryDirectory(persistentMemoryDirectory);
         return super.getClientConfig()
-                .setNativeMemoryConfig(memoryConfig);
+                .setNativeMemoryConfig(createNativeMemoryConfig());
     }
 }
--- hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/query/HDQueryBasicTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/impl/query/HDQueryBasicTest.java
@@ -1,58 +1,20 @@
 package com.hazelcast.map.impl.query;
 
 import com.hazelcast.config.Config;
-import com.hazelcast.enterprise.EnterpriseParallelParametersRunnerFactory;
+import com.hazelcast.enterprise.EnterpriseParallelJUnitClassRunner;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Collection;
 
 import static com.hazelcast.HDTestSupport.getHDConfig;
-import static com.hazelcast.internal.memory.impl.PersistentMemoryHeap.PERSISTENT_MEMORY_CHECK_DISABLED_PROPERTY;
-import static java.util.Arrays.asList;
 
-@RunWith(Parameterized.class)
-@Parameterized.UseParametersRunnerFactory(EnterpriseParallelParametersRunnerFactory.class)
+@RunWith(EnterpriseParallelJUnitClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class HDQueryBasicTest extends QueryBasicTest {
 
-    @Parameterized.Parameter
-    public String persistentMemoryDirectory;
-
-    @Parameterized.Parameters(name = "persistentMemoryDirectory: {0}")
-    public static Collection<Object[]> parameters() {
-        return asList(new Object[][]{
-                {PERSISTENT_MEMORY_DIRECTORY},
-                {null},
-        });
-    }
-
-    @BeforeClass
-    public static void init() {
-        System.setProperty(PERSISTENT_MEMORY_CHECK_DISABLED_PROPERTY, "true");
-    }
-
-    @AfterClass
-    public static void cleanup() {
-        System.clearProperty(PERSISTENT_MEMORY_CHECK_DISABLED_PROPERTY);
-    }
-
-    @Before
-    public void setUp() {
-        if (persistentMemoryDirectory != null) {
-            assumeThatLinuxOS();
-        }
-    }
-
     @Override
     protected Config getConfig() {
-        return getHDConfig(persistentMemoryDirectory);
+        return getHDConfig();
     }
-
 }

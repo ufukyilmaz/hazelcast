3d4ad557d0b963412aa02f473fa09bcf7108525f
Zoltán Baranyi <blazember@gmail.com>
2019-11-21 09:01:32 +0100

    Small metrics improvements (#3374)
    Two improvements/fixes:
    - **Ignore descriptor mutations in `extractMetrics()`**
    When decompressing and extracting a metric, mutating its
    `MetricDescriptor` impacts extracting the subsequent metric too if that
    metric shares at least one field with the modified one. This is fixed
    by copying the extracted descriptor into `lastDescriptor` before it is
    passed to the `MetricConsumer` where it can be modified.
    - **Use `metricString()` instead of `toString()` in `MetricsPlugin`**
    `MetricDescriptor.toString()` includes `excludedTargets` too, which is
    irrelevant info for the diagnostics. Therefore,
    `MetricDescriptor.metricString()` without `excludedTargets` is used
    instead of `toString()`.

--- hazelcast-enterprise/src/test/java/com/hazelcast/internal/diagnostics/EnterpriseProbeTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/internal/diagnostics/EnterpriseProbeTest.java
@@ -49,25 +49,25 @@ public class EnterpriseProbeTest extends AbstractDiagnosticsPluginTest {
         map.put("key", "value");
         map.put("key2", "value2");
         plugin.run(logWriter);
-        assertContains("[name=default,unit=count,metric=map.entryCount,excludedTargets={}]=2");
-        assertContains("[name=default,unit=count,metric=map.forceEvictionCount,excludedTargets={}]=");
-        assertContains("[name=default,unit=count,metric=map.usedMemory,excludedTargets={}]=");
+        assertContains("[name=default,unit=count,metric=map.entryCount]=2");
+        assertContains("[name=default,unit=count,metric=map.forceEvictionCount]=");
+        assertContains("[name=default,unit=count,metric=map.usedMemory]=");
 
         map.remove("key2");
         plugin.run(logWriter);
-        assertContains("[name=default,unit=count,metric=map.entryCount,excludedTargets={}]=1");
+        assertContains("[name=default,unit=count,metric=map.entryCount]=1");
 
         map.put("key", "changed_value");
         plugin.run(logWriter);
-        assertContains("[name=default,unit=count,metric=map.entryCount,excludedTargets={}]=1");
+        assertContains("[name=default,unit=count,metric=map.entryCount]=1");
 
         map.clear();
         plugin.run(logWriter);
-        assertContains("[name=default,unit=count,metric=map.entryCount,excludedTargets={}]=0");
+        assertContains("[name=default,unit=count,metric=map.entryCount]=0");
 
         map.put("new_key", "new_value");
         map.destroy();
         plugin.run(logWriter);
-        assertContains("[name=default,unit=count,metric=map.entryCount,excludedTargets={}]=0");
+        assertContains("[name=default,unit=count,metric=map.entryCount]=0");
     }
 }

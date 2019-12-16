ae768cfe2305b58395af2372149982b3cf107ac2
Josef Cacek <josef.cacek@gmail.com>
2019-12-16 15:40:19 +0200

    Remove internal Data interface usage from TokenCredentials (#3435)

--- hazelcast-enterprise/src/main/java/com/hazelcast/security/impl/TokenDeserializerImpl.java
+++ hazelcast-enterprise/src/main/java/com/hazelcast/security/impl/TokenDeserializerImpl.java
@@ -3,6 +3,7 @@ package com.hazelcast.security.impl;
 import static java.util.Objects.requireNonNull;
 
 import com.hazelcast.internal.serialization.SerializationService;
+import com.hazelcast.internal.serialization.impl.HeapData;
 import com.hazelcast.security.TokenCredentials;
 import com.hazelcast.security.TokenDeserializer;
 
@@ -16,6 +17,6 @@ public class TokenDeserializerImpl implements TokenDeserializer {
 
     @Override
     public Object deserialize(TokenCredentials tokenCredentials) {
-        return tokenCredentials != null ? serializationService.toObject(tokenCredentials.asData()) : null;
+        return tokenCredentials != null ? serializationService.toObject(new HeapData(tokenCredentials.getToken())) : null;
     }
 }

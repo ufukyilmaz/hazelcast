baac8be8fdde8f7fa247c54f2dad6a646731be59
Ahmet Mircik <ahmetmircik@gmail.com>
2020-01-10 16:06:58 +0300

    Ignore test using mock to rewrite it later

--- hazelcast-enterprise/src/test/java/com/hazelcast/internal/nearcache/impl/HDNearCacheNativeOOMETest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/internal/nearcache/impl/HDNearCacheNativeOOMETest.java
@@ -14,6 +14,7 @@ import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -26,6 +27,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(EnterpriseSerialJUnitClassRunner.class)
 @Category(QuickTest.class)
+@Ignore
 public class HDNearCacheNativeOOMETest extends HazelcastTestSupport {
 
     private IMap mainMap;

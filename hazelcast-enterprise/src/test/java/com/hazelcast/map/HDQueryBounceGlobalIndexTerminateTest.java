2de1e3a34515545ccdc5087cb460b04c44d77af8
petrpleshachkov <50169481+petrpleshachkov@users.noreply.github.com>
2020-08-17 14:07:18 +0200

    Fixed failing HDQueryBounceGlobalIndexTerminateTest (#3710)
    Adjusted unit test to use POOLed memory manager for the test
    
    Closes https://github.com/hazelcast/hazelcast-enterprise/issues/3684

--- hazelcast-enterprise/src/test/java/com/hazelcast/map/HDQueryBounceGlobalIndexTerminateTest.java
+++ hazelcast-enterprise/src/test/java/com/hazelcast/map/HDQueryBounceGlobalIndexTerminateTest.java
@@ -2,11 +2,15 @@ package com.hazelcast.map;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.enterprise.EnterpriseSerialJUnitClassRunner;
+import com.hazelcast.memory.MemorySize;
 import com.hazelcast.test.annotation.SlowTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import static com.hazelcast.HDTestSupport.getHDConfig;
+import static com.hazelcast.config.NativeMemoryConfig.MemoryAllocatorType.POOLED;
 import static com.hazelcast.map.HDQueryBounceTerminateTest.PredicateImpl;
+import static com.hazelcast.memory.MemoryUnit.MEGABYTES;
 import static com.hazelcast.query.impl.HDGlobalIndexProvider.PROPERTY_GLOBAL_HD_INDEX_ENABLED;
 
 @RunWith(EnterpriseSerialJUnitClassRunner.class)
@@ -15,7 +19,7 @@ public class HDQueryBounceGlobalIndexTerminateTest extends HDQueryBounceTest {
 
     @Override
     protected Config getConfig() {
-        Config config = super.getConfig();
+        Config config = getHDConfig(super.getConfig(), POOLED, new MemorySize(128, MEGABYTES));
         config.setProperty(PROPERTY_GLOBAL_HD_INDEX_ENABLED.getName(), "true");
         config.getSerializationConfig().addDataSerializableFactory(42, typeId -> new PredicateImpl());
         return config;

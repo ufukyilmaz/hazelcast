c712fb19ecc23225660fe813ecdbba7aed342a49
Zoltan Baranyi <blazember@gmail.com>
2020-10-05 16:56:31 +0200

    Perform zero size check in UnsafeMalloc
    We rely on a specific behavior of our LibMalloc implementations, that
    is, if the size to allocate is zero, we expect a NULL_ADDRESS returned.
    But Unsafe is not a safe API, and it may just forward the call to the
    underlying malloc()/realloc() implementation that behaves implementation
    dependent in this case and may allocate minimum size chunks.
    
    Then we fix this as checking the size in UnsafeMalloc and return
    NULL_ADDRESS in the given cases. Reallocate also frees the chunk.
    
    Fixes #3805

--- hazelcast-enterprise-native-memory/src/main/java/com/hazelcast/internal/memory/impl/UnsafeMalloc.java
+++ hazelcast-enterprise-native-memory/src/main/java/com/hazelcast/internal/memory/impl/UnsafeMalloc.java
@@ -6,6 +6,18 @@ public final class UnsafeMalloc implements LibMalloc {
 
     @Override
     public long malloc(long size) {
+        // we depend on returning NULL_ADDRESS if the size is 0
+        // Unsafe.allocateMemory() does not guarantee it
+        //
+        // on JDK implementations that just forward the call to malloc() the
+        // result is implementation dependent that may behave as allocating a
+        // chunk with the minimum-chunk-size bytes
+        //
+        // therefore, we perform the check ourselves to guarantee API stability
+        if (size == 0) {
+            return NULL_ADDRESS;
+        }
+
         try {
             return UNSAFE.allocateMemory(size);
         } catch (OutOfMemoryError e) {
@@ -15,6 +27,20 @@ public final class UnsafeMalloc implements LibMalloc {
 
     @Override
     public long realloc(long address, long size) {
+        // we depend on returning NULL_ADDRESS (and freeing the given memory
+        // block) if the size is 0
+        // Unsafe.reallocateMemory() does not guarantee this
+        //
+        // on JDK implementations that just forward the call to realloc() the
+        // result is implementation dependent that may behave as reallocating a
+        // chunk with the minimum-chunk-size bytes
+        //
+        // therefore, we perform the check ourselves to guarantee API stability
+        if (size == 0) {
+            UNSAFE.freeMemory(address);
+            return NULL_ADDRESS;
+        }
+
         try {
             return UNSAFE.reallocateMemory(address, size);
         } catch (OutOfMemoryError e) {

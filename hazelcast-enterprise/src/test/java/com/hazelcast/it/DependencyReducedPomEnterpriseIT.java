3c1c18d1da653c9f86af0b17b1315dda2517a085
Josef Cacek <josef.cacek@gmail.com>
2020-12-07 11:02:52 +0100

    Zero-dependency fix & tests (#3858)

---
+++ hazelcast-enterprise/src/test/java/com/hazelcast/it/DependencyReducedPomEnterpriseIT.java
@@ -0,0 +1,12 @@
+package com.hazelcast.it;
+
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.QuickTest;
+
+@RunWith(HazelcastParallelClassRunner.class)
+@Category(QuickTest.class)
+public class DependencyReducedPomEnterpriseIT extends DependencyReducedPomIT {
+}

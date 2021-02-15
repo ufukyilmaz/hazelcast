a216cfe6304fe3dc981b16d4f029f4f145c20ace
Bartok Jozsef <jozsi@hazelcast.com>
2021-02-15 11:21:17 +0200

    Revive Jet
    Revive Jet CLI

--- hazelcast-jet-all/src/test/resources/com/hazelcast/jet/server/testjob/src/main/java/com/hazelcast/jet/testjob/TestJob.java
+++ hazelcast-jet-all/src/test/resources/com/hazelcast/jet/server/testjob/src/main/java/com/hazelcast/jet/testjob/TestJob.java
@@ -18,6 +18,7 @@ package com.hazelcast.jet.testjob;
 
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.DAGInterface;
 import com.hazelcast.jet.core.AbstractProcessor;
 import com.hazelcast.jet.core.DAG;
 
@@ -27,7 +28,7 @@ public class TestJob {
         DAG dag = new DAG();
         dag.newVertex("v", StreamNoopProcessor::new);
         JetInstance instance = Jet.bootstrappedInstance();
-        instance.newJob(dag);
+        instance.newJob((DAGInterface) dag);
     }
 
     private static class StreamNoopProcessor extends AbstractProcessor {

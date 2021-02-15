de826173a80c571c1d6aefe3bf6b66b1c659b6e2
Ali Gurbuz <ali@hazelcast.com>
2021-02-15 18:00:45 +0300

    implement IdentifiedDataSerializable for some classes and mark as @SerializableByConvention others

--- hazelcast/src/main/java/com/hazelcast/jet/impl/aggregate/AggregateDataSerializerHook.java
+++ hazelcast/src/main/java/com/hazelcast/jet/impl/aggregate/AggregateDataSerializerHook.java
@@ -25,13 +25,35 @@ import static com.hazelcast.jet.impl.JetFactoryIdHelper.JET_AGGREGATE_DS_FACTORY
 
 public class AggregateDataSerializerHook implements DataSerializerHook {
 
+
+    public static final int FACTORY_ID = FactoryIdHelper.getFactoryId(JET_AGGREGATE_DS_FACTORY,
+            JET_AGGREGATE_DS_FACTORY_ID);
+
     /**
      * Serialization ID of the {@link AggregateOpAggregator} class.
      */
     public static final int AGGREGATE_OP_AGGREGATOR = 0;
 
-    public static final int FACTORY_ID = FactoryIdHelper.getFactoryId(JET_AGGREGATE_DS_FACTORY,
-            JET_AGGREGATE_DS_FACTORY_ID);
+    /**
+     * Serialization ID of the {@link AggregateOperationImpl} class.
+     */
+    public static final int AGGREGATE_OPERATION_IMPL = 1;
+
+    /**
+     * Serialization ID of the {@link AggregateOperation1Impl} class.
+     */
+    public static final int AGGREGATE_OPERATION_1_IMPL = 2;
+
+    /**
+     * Serialization ID of the {@link AggregateOperation2Impl} class.
+     */
+    public static final int AGGREGATE_OPERATION_2_IMPL = 3;
+
+    /**
+     * Serialization ID of the {@link AggregateOperation3Impl} class.
+     */
+    public static final int AGGREGATE_OPERATION_3_IMPL = 4;
+
 
     @Override
     public int getFactoryId() {
@@ -44,6 +66,14 @@ public class AggregateDataSerializerHook implements DataSerializerHook {
             switch (typeId) {
                 case AGGREGATE_OP_AGGREGATOR:
                     return new AggregateOpAggregator<>();
+                case AGGREGATE_OPERATION_IMPL:
+                    return new AggregateOperationImpl<>();
+                case AGGREGATE_OPERATION_1_IMPL:
+                    return new AggregateOperation1Impl<>();
+                case AGGREGATE_OPERATION_2_IMPL:
+                    return new AggregateOperation2Impl<>();
+                case AGGREGATE_OPERATION_3_IMPL:
+                    return new AggregateOperation3Impl<>();
                 default:
                     throw new IllegalArgumentException("Unknown type id " + typeId);
             }
